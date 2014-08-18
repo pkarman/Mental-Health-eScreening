78ac974061f3b9f79c25992dcf621518436971ed
michael gist <mgist.ctr@iiinfo.com>
2014-08-18 17:59:50 -0500

    #230

--- eScreeningDashboard/src/main/sql/sprint_21/26_sprint21_reference_data_templates.sql
+++ eScreeningDashboard/src/main/sql/sprint_21/26_sprint21_reference_data_templates.sql
@@ -547,3 +547,345 @@ ${MODULE_START}
 	WHERE template_id = 101;
 
 
+	
+	
+	
+ /* VETERAN SUMMARY -  TEMPLATES*/
+	
+-- /* VETERAN SUMMARY - Advance Directive  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (300, 6, 'Veteran Summary Advance Directive Entry', 'Veteran Summary Advance Directive Entry',
+'
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if score == 0>
+	<#assign scoreText = "Complete">
+	<#assign isComplete = true>
+<#elseif score == 1>
+	<#assign scoreText = "Declined">
+	<#assign isComplete = true>
+<#if>
+
+<#if isComplete>
+	
+	Advance Directive ${LINE_BREAK}
+	This is a legal paper that tells your wishes for treatment if you become too sick to talk, and if needed, can help your doctors and family to make decisions about your care. 
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendations: Call VA Social Work Service at (858) 552-8585 ext. 3500, and ask for help in creating and filing an advance directive. 
+</#if>
+${MODULE_END} ');
+INSERT INTO survey_template (survey_id, template_id) VALUES (9, 300);
+
+
+-- /* VETERAN SUMMARY - Homelessness  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (301, 6, 'Veteran Summary Homelessness Entry', 'Veteran Summary Homelessness Entry',
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if score == 0>
+	<#assign scoreText = "unstable housing">
+	<#assign isComplete = true>
+<#elseif score == 1>
+	<#assign scoreText = "stable housing">
+	<#assign isComplete = true>
+<#if>
+
+<#if isComplete>
+	
+	Homelessness ${LINE_BREAK}
+	This is when you do not have a safe or stable place you can return to every night. The VA is committed to ending Veteran homelessness by the end of 2015. 
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendation: Call the VA’s free National Call Center for Homeless Veterans at (877)-424-3838 and ask for help. Someone is always there to take your call.
+</#if>
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (7, 301);
+
+
+-- /* VETERAN SUMMARY - Alcohol Use  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (302, 6, 'Veteran Summary Alcohol Use Entry', 'Veteran Summary Alcohol Use Entry', 
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if (score >= 0) && (score <= 2)>
+	<#assign scoreText = "negative screen">
+	<#assign isComplete = true>
+<#elseif (score == 3) >
+	<#assign scoreText = "at risk">
+	<#assign isComplete = true>
+<#elseif (score >= 4) && (score <= 12)>
+	<#assign scoreText = "at risk">
+	<#assign isComplete = true>
+<#if>
+
+<#if isComplete>
+	
+	Alcohol Use ${LINE_BREAK}
+	Drinking too much, too often, or both, causes serious problems. Abuse can have negative effects on school, work, and relationships, and can cause liver disease and cirrhosis, congestive heart failure, seizures, falls, hypertension, and other serious health risks.
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendation: If female, limit yourself to one drink a day; if male, limit yourself to 2 drinks a day. If this is difficult, ask your clinician for help with managing your drinking.  
+</#if>
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (6, 302);
+
+
+-- /* VETERAN SUMMARY - Insomnia  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (303, 6, 'Veteran Summary Insomnia Entry', 'Veteran Summary Insomnia Entry', 
+'
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if (score >= 0) && (score <= 7)>
+	<#assign scoreText = "negative screen">
+	<#assign isComplete = true>
+<#elseif (score >= 8) && (score <= 14)>
+	<#assign scoreText = "at risk">
+	<#assign isComplete = true>
+<#elseif (score >= 4) && (score <= 12)>
+	<#assign scoreText = "at risk">
+	<#assign isComplete = true>
+<#if>
+
+<#if isComplete>
+	
+	Insomnia ${LINE_BREAK}
+	Insomnia is having trouble sleeping that lasts longer than a few weeks. Some causes are: medical (like depression or pain), lifestyle factors (such as too much caffeine), or even stress. 
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendation: Describe your sleeping problems to your clinician, or learn more about insomnia at the  CESAMH site at: http://escreening.cesamh.org 
+
+</#if>
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (36, 303);
+
+
+
+-- /* VETERAN SUMMARY - Environmental Exposure  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (304, 6, 'Veteran Summary Environmental Exposure Entry', 'Veteran Summary Environmental Exposure Entry', '
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if (score == 0)>
+	<#assign scoreText = "none reported">
+	<#assign isComplete = true>
+<#elseif (score == 1) >
+	<#assign scoreText = "at risk">
+	<#assign isComplete = true>
+<#if>
+
+<#if isComplete>
+	
+	Environmental Exposure ${LINE_BREAK}
+	This is when you have been exposed to a hazard that may have potential health risks.
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendation: Call Dale Willoughby at the Environmental Registry Program and discuss your exposure: (858) 642-3995, weekdays 7:30am-4:00pm. 
+
+
+</#if>
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (14, 304);
+
+
+
+-- /* VETERAN SUMMARY - Military Sexual Trauma (MST)  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (305, 6, 'Veteran Summary Military Sexual Trauma (MST) Entry', 'Veteran Summary Military Sexual Trauma (MST) Entry', 
+'
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if (score == 0)>
+	<#assign scoreText = "negative screen">
+	<#assign isComplete = true>
+<#elseif (score == 1)>
+	<#assign scoreText = "postive screen">
+	<#assign isComplete = true>
+<#elseif (score == 2)>
+	<#assign scoreText = "declined to answer">
+	<#assign isComplete = true>
+<#if>
+
+<#if isComplete>
+	
+	Military Sexual Trauma (MST) ${LINE_BREAK}
+	MST is sexual assault or repeated, threatening sexual harassment that occurred while the Veteran was in the military. MST can happen any time or anywhere, to men and women. MST can affect your physical and mental health, even years later.
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendation: Ask your clinician for help managing your MST. 
+
+</#if>
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (32, 305);
+
+
+
+-- /* VETERAN SUMMARY - Tobacco Use  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (306, 6, 'Veteran Summary Tobacco Use Entry', 'Veteran Summary Tobacco Use Entry', 
+' 
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if (score >= 0) && (score <= 2)>
+	<#assign scoreText = "negative screen">
+	<#assign isComplete = true>
+<#elseif (score == 2)>
+	<#assign scoreText = "current user">
+	<#assign isComplete = true>
+<#if>
+
+<#if isComplete>
+	
+	Tobacco Use ${LINE_BREAK}
+	The use of tobacco causes harm to nearly every organ in the body. Quitting greatly lowers your risk of death from cancers, heart disease, stroke, and emphysema. There are many options, such as in-person and telephone counseling, nicotine replacement, and prescription medications.
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendations: Prepare a plan to reduce or quit the use of tobacco. Get support from family and friends, and ask your clinician for help if needed.  
+
+</#if>
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (25, 306);
+
+
+
+
+
+-- /* VETERAN SUMMARY - Traumatic Brain Injury (TBI)  */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (307, 6, 'Veteran Summary Traumatic Brain Injury (TBI) Entry', 'Veteran Summary Traumatic Brain Injury (TBI) Entry',
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+
+${MODULE_TITLE_END}
+${MODULE_START}
+<#assign isComplete = false> <#-- fix when get right variables -->
+<#assign score = 0>
+<#assign scoreText = "">
+<#if (score >= 0) && (score <= 3)>
+	<#assign scoreText = "negative screen">
+	<#assign isComplete = true>
+<#elseif (score >= 4 && >
+	<#assign scoreText = "at risk">
+	<#assign isComplete = true>
+<<#if>
+
+<#if isComplete>
+	
+	Traumatic Brain Injury (TBI) ${LINE_BREAK}
+	A TBI is physical damage to your brain, caused by a blow to the head. Common causes are falls, fights, sports, and car accidents. A blast or shot can also cause TBI.
+	${LINE_BREAK}
+	${LINE_BREAK}
+	Results: ${scoreText}	${LINE_BREAK}
+	Recommendation: Ask your clinician about treatment for any symptoms that are bothering you. 
+
+</#if>
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (29, 307);
+
+
+
+-- /* VETERAN SUMMARY - My Depression Score */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (308, 6, 'Veteran Summary PHQ 9 Depression Entry', 'Veteran Summary PHQ 9 Depression Entry', 
+'
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+Depression
+${MODULE_TITLE_END}
+${MODULE_START}
+Depression is when you feel sad and hopeless for much of the time. It affects your body and thoughts, and interferes with daily life. There are effective treatments and resources for dealing with depression.${LINE_BREAK}
+Recommendation: Ask your clinician for further evaluation and treatment options. 
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (30, 308);
+
+
+
+-- /* VETERAN SUMMARY -  My Pain Score  (Basic Pain) */
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (309, 6, 'Veteran Summary Basic Pain Score Entry', 'Veteran Summary Basic Pain Score Entry', 
+'
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+Pain 
+${MODULE_TITLE_END}
+${MODULE_START}
+Pain can slow healing and stop you from being active. Untreated pain can harm your sleep, outlook, and ability to do things. ${LINE_BREAK}
+Recommendation: Tell your clinician if medications aren’t reducing your pain, or if the pain suddenly increases or changes, and ask for help with managing your pain. 
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (20, 309);
+
+
+
+-- /* VETERAN SUMMARY -  My PTSD Score*/
+INSERT INTO template(template_id, template_type_id, name, description, template_file) VALUES (310, 6, 'Veteran Summary PTSD Entry', 'Veteran Summary PTSD Entry',
+'
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+PTSD 
+${MODULE_TITLE_END}
+${MODULE_START}
+PTSD is when remembering a traumatic event keeps you from living a normal life. It’s also called shell shock or combat stress. Common symptoms include recurring memories or nightmares of the event, sleeplessness, and feeling angry, irritable, or numb. ${LINE_BREAK}
+Recommendation: Ask your clinician for further evaluation and treatment options. 
+${MODULE_END}
+');
+INSERT INTO survey_template (survey_id, template_id) VALUES (35, 310);
