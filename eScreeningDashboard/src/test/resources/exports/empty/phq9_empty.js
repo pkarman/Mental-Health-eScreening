ce6f3dfe6395038c40f13e55fd05a589669108bf
Robin Carnow <rcarnow@iiinfo.com>
2014-08-19 13:55:07 -0400

    #230 phq9 empty test file didn't have the last changes in last commit

--- eScreeningDashboard/src/test/resources/exports/empty/phq9_empty.js
+++ eScreeningDashboard/src/test/resources/exports/empty/phq9_empty.js
@@ -3,17 +3,7 @@
 	testSurveys : [ {
 		surveyName : "PHQ-9",
 		smrMap : {
-			dep1_interest : "2",
-			dep2_down : "1",
-			dep3_sleep : "3",
-			dep4_tired : "1",
-			dep5_appetite : "2",
-			dep6_feelbad : "1",
-			dep7_concentrate : "2",
-			dep8_moveslow : "2",
-			dep9_dead : "1",
-			dep10_difficult : "1",
-			dep_score_phq9 : "15"
+			
 		}
 	} ]
 }
