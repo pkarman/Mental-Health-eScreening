0c70f347821b451a9647bd4e0b13339e4a55f6b7
Robin Carnow <rcarnow@iiinfo.com>
2014-08-22 14:50:26 -0400

    #553 Fixed question answer noted in ticket by Liz so it is not a other type answer

--- eScreeningDashboard/src/main/sql/sprint_21/25_sprint21_reference_data_modules.sql
+++ eScreeningDashboard/src/main/sql/sprint_21/25_sprint21_reference_data_modules.sql
@@ -5,3 +5,7 @@
 update measure set parent_measure_id = null where measure_id=382;
 INSERT INTO survey_page_measure (survey_page_id, measure_id, display_order) VALUES (56, 382, 0);
 Update survey_page_measure set display_order=1 where survey_page_id=56 and measure_id=381;
+
+
+UPDATE measure_answer SET answer_type=NULL where measure_answer_id=4136
+
