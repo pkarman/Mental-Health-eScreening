e9cd7831d7dd5655443b66cc2a8721dffe7cf08c
Tonté Pouncil <pouncilt.developer@gmail.com>
2014-08-27 00:33:41 -0500

    #588 - Added support for save and cancel functionality.

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/questions/selectMultipleMatrixQuestionController.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/questions/selectMultipleMatrixQuestionController.js
@@ -5,17 +5,14 @@ Editors.controller('selectMultipleMatrixQuestionController', ['$rootScope', '$sc
 	$scope.question = EScreeningDashboardApp.models.Question.toUIObjects($rootScope.selectedQuestion);
     $scope.answerTypeMenuOptions = answerTypeMenuOptions;
 	$scope.isDirty = false;
-	$scope.types = [{name:'Regular'},{name:'Other'},{name:'None'}];
+    $scope.parentSave = $scope.save;
 	
 	$scope.blur = function(){
 		$scope.isDirty = true;
 	};
 	
 	$scope.save = function(){
-		// Not sure exactly what we need to be doing here quite yet.
-		EscreeningDashboardApp.services.QuestionService.update(QuestionService.setUpdateQuestionRequestParameter($scope.question)).then(function(question){
-			// Process this.
-		});
+        $scope.parentSave();
 	};
 	
 	$scope.cancel = function(){
