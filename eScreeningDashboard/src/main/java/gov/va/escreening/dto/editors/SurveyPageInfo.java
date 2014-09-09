153da43d65be58d49b7af3d0df07b7c8b5256837
Tonté Pouncil <pouncilt.developer@gmail.com>
2014-09-09 14:52:44 -0500

    #516- Added the id attribute and getter and setter methods.

--- eScreeningDashboard/src/main/java/gov/va/escreening/dto/editors/SurveyPageInfo.java
+++ eScreeningDashboard/src/main/java/gov/va/escreening/dto/editors/SurveyPageInfo.java
@@ -6,12 +6,21 @@ import java.util.List;
  * Created by pouncilt on 9/9/14.
  */
 public class SurveyPageInfo {
+    private Integer id;
     private String description;
     private int pageNumber;
     private String title;
     private List<QuestionInfo> questions;
 
     public SurveyPageInfo() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getDescription() {
         return description;
@@ -47,6 +56,6 @@ public class SurveyPageInfo {
 
     @Override
     public String toString() {
-        return "SurveyPageInfo [description=" + description + ", pageNumber=" + pageNumber + ", title=" + title + ", questions=" + questions + "]";
+        return "SurveyPageInfo [id=" + id + ", description=" + description + ", pageNumber=" + pageNumber + ", title=" + title + ", questions=" + questions + "]";
     }
 }
