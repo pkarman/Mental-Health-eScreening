786250bafdbbc10b049e392cf465b58bbe201445
kjsolution <kliu@kais-mbp.gateway.2wire.net>
2014-09-16 21:37:50 -0700

    T642: New DTO files for template CRUD

---
+++ eScreeningDashboard/src/main/java/gov/va/escreening/dto/TemplateDTO.java
@@ -0,0 +1,66 @@
+package gov.va.escreening.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class TemplateDTO {
+	private Integer templateId;
+    private String name;
+    private String description;
+    
+    private String templateFile;
+    private Date dateCreated;
+    private List<VariableTemplateDTO> variableTemplateList;
+    private Integer templateTypeId;
+    private boolean isGraphical;
+    
+	public Integer getTemplateId() {
+		return templateId;
+	}
+	public void setTemplateId(Integer templateId) {
+		this.templateId = templateId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTemplateFile() {
+		return templateFile;
+	}
+	public void setTemplateFile(String templateFile) {
+		this.templateFile = templateFile;
+	}
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public List<VariableTemplateDTO> getVariableTemplateList() {
+		return variableTemplateList;
+	}
+	public void setVariableTemplateList(List<VariableTemplateDTO> variableTemplateList) {
+		this.variableTemplateList = variableTemplateList;
+	}
+	public Integer getTemplateTypeId() {
+		return templateTypeId;
+	}
+	public void setTemplateType(Integer templateTypeId) {
+		this.templateTypeId = templateTypeId;
+	}
+	public boolean isGraphical() {
+		return isGraphical;
+	}
+	public void setGraphical(boolean isGraphical) {
+		this.isGraphical = isGraphical;
+	}
+
+}
