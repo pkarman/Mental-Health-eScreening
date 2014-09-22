cc6d4f192e4d203fac8b777229f1b303aca5d17b
Khalid R. Rizvi <javaguru786@gmail.com>
2014-09-21 21:05:17 -0400

    t658 support for excel view

---
+++ eScreeningDashboard/src/main/java/gov/va/escreening/view/DataDictionaryExcelView.java
@@ -0,0 +1,116 @@
+package gov.va.escreening.view;
+
+import gov.va.escreening.dto.dashboard.DataExportCell;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.view.document.AbstractExcelView;
+
+import com.google.common.collect.Table;
+
+public class DataDictionaryExcelView extends AbstractExcelView {
+
+	private static final Logger logger = LoggerFactory.getLogger(DataDictionaryExcelView.class);
+
+	private final String defaultErrorMsg = "An error occured while generating the requested export.  Please contact technical support for assistance.";
+
+	@Override
+	protected void buildExcelDocument(Map model, HSSFWorkbook workbook,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		Map<String, Table<Integer, String, String>> dataDictionary = (Map<String, Table<Integer, String, String>>) model.get("dataDictionary");
+
+		for (String surveyName : dataDictionary.keySet()) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Survey name:" + surveyName);
+			}
+
+			HSSFSheet sheet = workbook.createSheet(surveyName);
+
+			Table<Integer, String, String> measuresTable = dataDictionary.get(surveyName);
+			setExcelHeader(surveyName, sheet, measuresTable.columnKeySet());
+			setExcelRows(surveyName, sheet, measuresTable);
+		}
+
+	}
+
+	private void setExcelHeader(String surveyName, HSSFSheet excelSheet,
+			Set<String> headerColumns) {
+
+		int row = 0;
+		HSSFRow excelHeader = excelSheet.createRow(row);
+		excelHeader.createCell(0).setCellValue(surveyName);
+
+		excelHeader = excelSheet.createRow(row + 2);
+
+		int columnIndex = 0;
+		for (String header : headerColumns) {
+			excelHeader.createCell(columnIndex).setCellValue(header);
+			columnIndex++;
+		}
+	}
+
+	private void setExcelRows(String surveyName, HSSFSheet excelSheet,
+			Table<Integer, String, String> measuresTable) {
+
+		int row = 4;
+		for (Integer measureRowNum : measuresTable.rowKeySet()) {
+			StringBuilder debugString = new StringBuilder();
+			int columnIndex = 0;
+			HSSFRow excelRow = excelSheet.createRow(row++);
+			
+			for (Entry<String, String> rowData : measuresTable.row(measureRowNum).entrySet()) {
+				debugString.append(String.format("%s:%s$", rowData.getKey(), rowData.getValue()));
+				
+				excelRow.createCell(columnIndex).setCellValue(rowData.getValue());
+				columnIndex++;
+			}
+			
+			if (logger.isDebugEnabled()) {
+				logger.debug(debugString.toString());
+			}
+		}
+	}
+
+	// private void replaceSpreadSheetContentWithMessage(HSSFWorkbook workbook,
+	// String message) {
+	//
+	// // remove any existing sheets
+	// int numSheets = workbook.getNumberOfSheets();
+	// if (numSheets > 0) {
+	// for (int index = 0; index < numSheets; index++) {
+	// workbook.removeSheetAt(index);
+	// }
+	// }
+	//
+	// HSSFSheet excelSheet = workbook.createSheet("Assessment Data Export");
+	// HSSFRow excelRow = excelSheet.createRow(0);
+	// excelRow.createCell(0).setCellValue(message);
+	// }
+	//
+	// private boolean validateCellMatchesColumn(int columnIndex,
+	// DataExportCell cell, HSSFRow header) {
+	// if (header.getCell(columnIndex) == null)
+	// throw new DataExportException(String.format("A column does not exist for the referenced index of: %s",
+	// columnIndex));
+	//
+	// HSSFCell headerColumn = header.getCell(columnIndex);
+	// String headerName = headerColumn.getStringCellValue().toLowerCase();
+	// if (!cell.getColumnName().equalsIgnoreCase(headerName))
+	// throw new DataExportException(String.format("The cell name of: %s did not match the column name of: %s.",
+	// cell.getColumnName().toLowerCase(), headerName.toLowerCase()));
+	//
+	// return true;
+	// }
+}
