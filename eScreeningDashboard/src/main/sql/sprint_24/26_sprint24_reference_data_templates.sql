23ce7bc241ff7a0b34bd7c653f48e54937e9a7d7
iiiming <mzhu.ctr@iiinfo.com>
2014-10-01 15:41:01 -0400

    more fixes to cprs notes -- skip logic

--- eScreeningDashboard/src/main/sql/sprint_24/26_sprint24_reference_data_templates.sql
+++ eScreeningDashboard/src/main/sql/sprint_24/26_sprint24_reference_data_templates.sql
@@ -138,9 +138,9 @@ ${MODULE_START}
 			<#if typeIsComplete>
 				<#if isFirst>
 					<#assign isFirst=false>	
-					The Veteran reported previous enlistment in the ${type} 
+					The Veteran reported entering the ${type} 
 				<#else>
-					${nextLine}The Veteran reported entering the ${type} 
+					${nextLine}The Veteran reported previous enlistment in the ${type}
 				</#if>
 				<#if branchIsComplete> ${NBSP}${branch}</#if>
 				<#if beg_yrIsComplete> ${NBSP}in ${beg_yr}</#if>
@@ -163,4 +163,172 @@ ${MODULE_START}
 ${MODULE_END}
 '
 where template_id = 12;
+
+/** Update OIR/OEF skip logic ***/
+UPDATE template SET template_file = 
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+OEF/OIF CLINICAL REMINDER:
+${MODULE_TITLE_END}
+${MODULE_START}
+	<#if (var660.children)?? && ((var660.children)?size > 0)  >
+	  <#if hasValue(getSelectMultiDisplayText(var660)) >
+    	<#if isSelectedAnswer(var660, var661)> <#-- no -->
+			The Veteran\'s most recent deployment was not for Operation Enduring Freedom (OEF) or Operation Iraqi Freedom (OIF)
+    		
+		<#elseif isSelectedAnswer(var660, var659)><#-- yes -->
+    			The Veteran\'s most recent deployment was for " + ${getSelectOneDisplayText(var660)}
+    			<#if var683?? && getSelectOneDisplayText(var683) != "notfound"> ${NBSP}in ${getSelectOneDisplayText(var683)}</#if>   		
+		<#elseif isSelectedAnswer(var660, var662)><#-- yes -->
+    		The Veteran\'s most recent deployment was for ${getSelectOneDisplayText(var660)}
+    		<#if var663?? && getSelectOneDisplayText(var663) != "notfound"> ${NBSP}in ${getSelectOneDisplayText(var663)}</#if>
+    	</#if>
+    	. ${NBSP}
+    </#if>
+	<#else>
+		${getNotCompletedText()}.
+	</#if>
+${MODULE_END}'
+where template_id = 13;
+
+update template 
+set template_file = '
+<#include "clinicalnotefunctions">
+ <#-- Template start --> ${MODULE_TITLE_START} MILITARY DEPLOYMENTS AND HISTORY: ${MODULE_TITLE_END} 
+ ${MODULE_START} <#assign deployments_section>  
+ <#assign allComplete = false> 
+ <#assign allAnswersNone = false> 
+ 
+  <#-- var5000 --> 
+  <#assign Q1_text = ""> <#assign Q1_complete = false> <#assign Q1_isAnswerNone = false>
+  <#if (var5001.value)??> 
+  		<#if var5001.value = "true"> 
+  			<#assign Q1_isAnswerNone = true> 
+  			<#assign Q1_complete = true> 
+  		<#else> 
+  			<#assign Q1_isAnswerNone = false> 
+  		</#if> 
+  </#if> 
+  
+  <#if !Q1_isAnswerNone && !Q1_complete && (var5000.children)?? && ((var5000.children)?size > 0)> 
+  		<#assign Q1_complete = true> 
+  </#if>  
+  
+  <#-- var5020 --> 
+  <#assign Q2_text = ""> 
+  <#assign Q2_complete = false> 
+  <#assign Q2_isAnswerNone = false>
+   <#if (var5021.value)??> 
+   		<#if var5021.value = "true"> 
+   			<#assign Q2_isAnswerNone = true> <#assign Q2_complete = true> 
+   		<#else> <#assign Q2_isAnswerNone = false> 
+   		</#if> 
+   </#if> 
+   <#if !Q2_isAnswerNone && !Q2_complete && (var5020.children)?? && ((var5020.children)?size > 0)> 
+   		<#assign Q2_complete = true> 
+   </#if>  
+   
+   <#-- var5130 --> 
+   <#assign Q3_text = ""> <#assign Q3_complete = false> <#assign Q3_isAnswerNone = false> 
+   
+   <#if (var5131.value)??>
+        <#assign Q3_complete = true> 
+   		<#if var5131.value = "true"> 
+   			<#assign Q3_isAnswerNone = true>
+   		<#else> 
+   			<#assign Q3_isAnswerNone = false> 
+   		</#if> 
+   </#if>
+    
+   <#assign counter = 0> 
+   <#assign all_rows = "">  
+   <#if !Q3_isAnswerNone && (var5130.children)?? && (var5130.children)?has_content> 
+   		<#assign rows = {}> 
+   
+   		<#list ((var5130.children)![]) as v> 
+   			<#if (v.children)?? > 
+   				<#list v.children as c> 
+   					<#if (c.row)??> 
+   						<#assign beg_months = ["var5061", "var5062", "var5063", "var5064", "var5065", "var5066", "var5067", "var5068", "var5069", "var5070", "var5071", "var5072"]>  
+   						<#assign end_months = ["var5101", "var5102", "var5103", "var5104", "var5105", "var5106", "var5107", "var5108", "var5109", "var5110", "var5111", "var5112"]>  
+   
+   						<#if beg_months?seq_contains(c.key)> <#assign row_key = "var5060"> 
+   						<#elseif end_months?seq_contains(c.key)> 
+   							<#assign row_key = "var5100"> 
+   						<#else> <#assign row_key = (c.key)?string> 
+   						</#if>  
+   						<#assign row_idx = (c.row)?string> 
+   						<#assign row_value = ((c.displayText)!"")?string> 
+   						<#assign r ={}> 
+   						<#assign row_name = ("row" + row_idx + "_" + row_key)?string > 
+   						<#assign rows =  rows + {row_name:row_value}> 
+   					</#if> 
+   				</#list> 
+   			</#if> 
+   		</#list>  
+   		<#assign uniqueRows = []> 
+   		<#assign e = []> 
+   		<#if (rows?keys)??> 
+   			<#list (rows?keys?sort) as k> 
+   				<#assign e = (k?split("_"))> 
+   				<#if !(uniqueRows?seq_contains(e[0]))> 
+   					<#assign uniqueRows = uniqueRows + [e[0]]> 
+   				</#if> 
+   			</#list> 
+   		</#if>  
+   		<#assign outputText = "">  
+   		<#if uniqueRows?has_content>
+    		<#assign innerNextLine = ""> 
+    		<#list uniqueRows as r> 
+    			<#assign loc = (rows[r + "_" + "var5041"])!"">  
+    			<#assign beg_month = (rows[r + "_" + "var5060"])!""> 
+    			<#assign beg_yr = (rows[r + "_" + "var5081"])!"">  
+    			<#assign end_month = (rows[r + "_" + "var5100"])!""> 
+    			<#assign end_yr = (rows[r + "_" + "var5121"])!"">  
+    			<#if (loc?has_content) && (beg_month?has_content) && (beg_yr?has_content) && (end_month?has_content) && (end_yr?has_content)> ${innerNextLine}
+    				<#assign all_rows = all_rows +	beg_month + "/" + beg_yr + "-"  + end_month + "/" + end_yr + ": "  + loc + LINE_BREAK> 
+    				<#assign innerNextLine = "${LINE_BREAK}"> 
+    				<#assign counter = counter + 1>  
+    			<#else> 
+    			<#-- if Q3 is missing any answers, reset everything  and set complete = false--> 
+    				<#assign Q3_complete = false> <#assign all_rows = ""> <#assign counter = 0> 
+    				<#break> 
+    			</#if> 
+    		</#list> 
+    		<#if (counter > 0)> 
+    			<#assign Q3_complete = true> 
+    		</#if> 
+    	</#if> 
+    </#if>  
+    
+    <#if Q1_isAnswerNone && Q2_isAnswerNone && Q3_isAnswerNone> <#assign allAnswersNone = true> 
+    </#if>     
+    <#assign nextLine = ""> 
+   	<#if allAnswersNone> ${getAnsweredNoneAllText("Military Deployments History")} 
+   	<#else> 
+    	<#if Q1_isAnswerNone> The Veteran reported receiving no disciplinary actions. <#assign nextLine = "${LINE_BREAK}${LINE_BREAK}"> 
+    	<#elseif Q1_complete> The Veteran reported receiving the following disciplinary actions: ${getSelectMultiDisplayText(var5000)}. 
+  			<#assign nextLine = "${LINE_BREAK}${LINE_BREAK}"> 
+   		</#if>  
+    
+   		<#if Q2_isAnswerNone> ${nextLine}The Veteran reported receiving no personal awards or commendations. <#assign nextLine = "${LINE_BREAK}${LINE_BREAK}">     		
+   		<#elseif Q2_complete> ${nextLine}The Veteran reported receiving the following personal awards or commendations: ${getSelectMultiDisplayText(var5020)}. 
+    			<#assign nextLine = "${LINE_BREAK}${LINE_BREAK}"> 
+    	</#if>   
+    
+   		<#if (counter <= 1)> 
+   			<#assign frag = counter + " time"> <#assign frag2 = "area"> <#assign frag3 = "period"> 
+   		<#else> 
+   			<#assign frag = counter + " times"> <#assign frag2 = "areas"> <#assign frag3 = "periods">     		
+   		</#if> 		
+   		${nextLine}The Veteran was deployed ${frag}
+   		<#if (counter>0)> to the following ${frag2} during the following time ${frag3}:${LINE_BREAK}${LINE_BREAK} ${all_rows} 
+   		<#else>.
+   		</#if>
+    </#if>  
+    </#assign> <#if !(deployments_section = "") > ${deployments_section} <#else> ${noParagraphData} </#if> 
+    ${MODULE_END} 
+    '
+where template_id = 14;
 
