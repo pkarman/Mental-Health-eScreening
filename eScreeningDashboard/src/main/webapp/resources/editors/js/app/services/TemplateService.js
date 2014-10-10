255c75a91a6f741ac0f825506d4026037b183064
Tonté Pouncil <pouncilt.developer@gmail.com>
2014-10-10 15:36:33 -0500

    #642 - Implemented the Restangular framework for the Templates editor; but still need to handle error message.
    Renamed the job variable to service.

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/services/TemplateService.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/services/TemplateService.js
@@ -7,14 +7,13 @@ angular.module('EscreeningDashboardApp.services.template', ['restangular'])
     .factory('TemplateService', ['Restangular', 'TemplateType', function (Restangular, TemplateType){
         "use strict";
 
-        var job = Restangular.service("services/templateTypes");
-        //var job = Restangular.all('services/templateTypes');
+        var service = Restangular.service("services/templateTypes");
 
         Restangular.extendModel("services/templateTypes", function(model) {
             return angular.extend(model, TemplateType);
         });
         
         // Expose the public TemplateService API to the rest of the application.
-        return job;
+        return service;
     }]);
         
