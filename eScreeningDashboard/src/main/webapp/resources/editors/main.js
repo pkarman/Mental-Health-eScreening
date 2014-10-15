cae7c248c299f26fe3bceed9a0af98deeb38991d
Tonté Pouncil <pouncilt.developer@gmail.com>
2014-10-15 08:18:21 -0500

    #642 - Removed Restangular global configuration.

--- eScreeningDashboard/src/main/webapp/resources/editors/main.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/main.js
@@ -43,25 +43,18 @@ Editors.directive('ngReallyClick', [function() {
         }
     };
 }]);
-Editors.config(function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('/escreeningdashboard/dashboard');
-    RestangularProvider.setRequestSuffix('.json');
-});
 
 Editors.run(function(editableOptions) {
     editableOptions.theme = 'bs3';
 });
 
-Editors.run(
-    [        '$rootScope', '$state', '$stateParams', '$modal',
-        function ($rootScope,   $state,   $stateParams,  $modal) {
-
-            // It's very handy to add references to $state and $stateParams to the $rootScope
-            // so that you can access them from any scope within your applications.For example,
-            // <li ng-class="{ active: $state.includes('assessments.list') }"> will set the <li>
-            // to active whenever 'assessments.list' or one of its descendents is active.
-            $rootScope.$state = $state;
-            $rootScope.$stateParams = $stateParams;
+Editors.run(['$rootScope', '$state', '$stateParams', '$modal', function ($rootScope,   $state,   $stateParams,  $modal) {
+    // It's very handy to add references to $state and $stateParams to the $rootScope
+    // so that you can access them from any scope within your applications.For example,
+    // <li ng-class="{ active: $state.includes('assessments.list') }"> will set the <li>
+    // to active whenever 'assessments.list' or one of its descendents is active.
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
 
 }]);
 Editors.value('MessageHandler', new BytePushers.models.MessageHandler());
