2f92b26cc3657b112db609b06d5b44abcd993378
iiiming <mzhu.ctr@iiinfo.com>
2014-10-15 15:12:42 -0400

    completed TBI skip logic

--- eScreeningDashboard/src/main/sql/sprint_24/27_sprint24_reference_data_template_variables.sql
+++ eScreeningDashboard/src/main/sql/sprint_24/27_sprint24_reference_data_template_variables.sql
@@ -164,4 +164,8 @@ INSERT INTO assessment_var_children(assessment_var_children_id, variable_parent,
 INSERT INTO assessment_var_children(assessment_var_children_id, variable_parent, variable_child) VALUES (932, 10719, 10717);
 INSERT INTO assessment_var_children(assessment_var_children_id, variable_parent, variable_child) VALUES (933, 10719, 10718);
 
-
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2047, 30);
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (10714, 30);
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (10715, 30);
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (10716, 30);
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (10717, 30);
