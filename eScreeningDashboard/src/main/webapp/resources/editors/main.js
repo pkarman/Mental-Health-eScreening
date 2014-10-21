c575cbef475b5f30c5889e927ba3ddeef405208a
Aaron Roberson <aaronaroberson@gmail.com>
2014-10-21 09:14:38 -0700

    #660 Started edit variable functionality

--- eScreeningDashboard/src/main/webapp/resources/editors/main.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/main.js
@@ -72,26 +72,101 @@ Editors.config(function(RestangularProvider, $provide) {
 		// register the tool with textAngular
 		taRegisterTool('addVariable', {
 			display: '<button title="Add Variable" class="btn btn-default"><i class="fa fa-plus"></i> Add Variable</button>',
+			tooltiptext: {
+				tooltip: 'Insert / edit variable'
+			},
 			action: function(){
 
-				var assementVariable;
+				var assessmentVariable = {
+					id: 287,
+					typeId: '1',
+					name: 'demo_phone',
+					displayName: 'phone',
+					answerId: 15,
+					measureId: 62,
+					measureTypeId: 93
+				};
 
 				var modalInstance = $modal.open({
 					template: '<mhe-assessment-variables assessment-variable="assessmentVariable"></mhe-assessment-variables>',
-					controller: function($scope, $modalInstance) {
+					controller: function($scope, $modalInstance, $timeout) {
+
+						$timeout(function() {
+							$modalInstance.close();
+						}, 5000);
 
 					}
-
 				});
+
+				var embed = '<code class="ta-insert-variable">(' + assessmentVariable.name + ')</code>';
+
+				return this.$editor().wrapSelection('insertHTML', embed, true);
+
+			},
+			activeState: function(commonElement){
+				var result = false;
+				console.log('commonElement', commonElement);
+				return this.$editor().queryCommandState('ta-insert-variable');
 			},
 			onElementSelect: {
-				element: 'img',
-				onlyWithAttrs: ['ta-insert-video']
-				//action: imgOnSelectAction
+				element: 'code',
+				action: function (event, $element, editorScope) {
+					// setup the editor toolbar
+					// Credit to the work at http://hackerwins.github.io/summernote/ for this editbar logic
+					event.preventDefault();
+					editorScope.displayElements.popover.css('width', '435px');
+					var container = editorScope.displayElements.popoverContainer;
+					container.empty();
+					container.css('line-height', '28px');
+					var link = angular.element('<a href="' + $element.attr('href') + '" target="_blank">' + $element.attr('href') + '</a>');
+					link.css({
+						'display': 'inline-block',
+						'max-width': '200px',
+						'overflow': 'hidden',
+						'text-overflow': 'ellipsis',
+						'white-space': 'nowrap',
+						'vertical-align': 'middle'
+					});
+					//container.append(link);
+					var buttonGroup = angular.element('<div class="btn-group pull-right">');
+					var reLinkButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on"><i class="fa fa-edit icon-edit"></i></button>');
+					reLinkButton.on('click', function (event) {
+						event.preventDefault();
+						var urlLink = window.prompt(taTranslations.insertLink.dialogPrompt, $element.attr('href'));
+						if (urlLink && urlLink !== '' && urlLink !== 'http://') {
+							$element.attr('href', urlLink);
+							editorScope.updateTaBindtaTextElement();
+						}
+						editorScope.hidePopover();
+					});
+					buttonGroup.append(reLinkButton);
+					var unLinkButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on"><i class="fa fa-unlink icon-unlink"></i></button>');
+					// directly before this click event is fired a digest is fired off whereby the reference to $element is orphaned off
+					unLinkButton.on('click', function (event) {
+						event.preventDefault();
+						$element.replaceWith($element.contents());
+						editorScope.updateTaBindtaTextElement();
+						editorScope.hidePopover();
+					});
+					buttonGroup.append(unLinkButton);
+					var targetToggle = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on">Open in New Window</button>');
+					if ($element.attr('target') === '_blank') {
+						targetToggle.addClass('active');
+					}
+					targetToggle.on('click', function (event) {
+						event.preventDefault();
+						$element.attr('target', ($element.attr('target') === '_blank') ? '' : '_blank');
+						targetToggle.toggleClass('active');
+						editorScope.updateTaBindtaTextElement();
+					});
+					buttonGroup.append(targetToggle);
+					container.append(buttonGroup);
+					editorScope.showPopover($element);
+				}
 			}
 		});
 		// add the button to the default toolbar definition
-		$delegate.toolbar[4] = ['addVariable'];
+		$delegate.toolbar[$delegate.toolbar.length] = ['addVariable'];
 		return $delegate;
 	}]);
 });
