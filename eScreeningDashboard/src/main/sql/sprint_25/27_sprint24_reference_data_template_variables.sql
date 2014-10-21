7638bae07651534deee9fd15e8c7c6baf4341450
Khalid R. Rizvi <javaguru786@gmail.com>
2014-10-21 14:19:42 -0400

    t699 hyp_criteria formula added

--- eScreeningDashboard/src/main/sql/sprint_25/27_sprint24_reference_data_template_variables.sql
+++ eScreeningDashboard/src/main/sql/sprint_25/27_sprint24_reference_data_template_variables.sql
@@ -2,3 +2,12 @@ UPDATE assessment_variable SET formula_template= '((([2010]?1:0) + ([2011]?1:0) 
 UPDATE assessment_variable SET formula_template= '((([2017]?1:0) + ([2018]?1:0) + ([2019]?1:0) + ([2020]?1:0) + ([2021]?1:0))>=1?1:0)' where assessment_variable_id=10715;
 UPDATE assessment_variable SET formula_template= '((([2023]?1:0) + ([2024]?1:0) + ([2025]?1:0) + ([2027]?1:0) + ([2028]?1:0) + ([2029]?1:0))>=1?1:0)' where assessment_variable_id=10716;
 UPDATE assessment_variable SET formula_template= '((([2031]?1:0) + ([2032]?1:0) + ([2033]?1:0) + ([2034]?1:0) + ([2035]?1:0) + ([2036]?1:0))>=1?1:0)' where assessment_variable_id=10717;
+
+/*
+if hyp_score >= 7 && hyp2_time ==1 && hyp3_problem >=2 then hyp_criteria=1 else 0
+*/
+
+INSERT INTO assessment_variable(assessment_variable_id, assessment_variable_type_id, display_name, description, formula_template) VALUES (10720, 4, 'hyp_criteria', 'hyp_criteria formula', '(([$2809$])>=7 && ([2790])==1 && ([2800])>=2)?1:0');
+INSERT INTO assessment_var_children(assessment_var_children_id, variable_parent, variable_child) VALUES (940, 10720, 2809);
+INSERT INTO assessment_var_children(assessment_var_children_id, variable_parent, variable_child) VALUES (941, 10720, 2790);
+INSERT INTO assessment_var_children(assessment_var_children_id, variable_parent, variable_child) VALUES (942, 10720, 2800);
--- eScreeningDashboard/src/test/java/gov/va/escreening/xport/FormulaExpressionTest.java
+++ eScreeningDashboard/src/test/java/gov/va/escreening/xport/FormulaExpressionTest.java
@@ -79,16 +79,38 @@ public class FormulaExpressionTest {
 		ExpressionParser parser = new SpelExpressionParser();
 		String mathFormula = "((true ? 1:0) + (true?1:0) + (true?1:   0) + (true ?1 :0) + (true?1:0) + (true ? 1 :   0))   >=   1?1:0";
 		Assert.assertTrue(parser.parseExpression(mathFormula).getValue(String.class).equals("1"));
-		//String businessFormula = mathFormula.replaceAll("[$]", "").replaceAll("[?]", "").replaceAll("1\\s*:\\s*0", "").replaceAll(">=\\s*1", "");
+		// String businessFormula = mathFormula.replaceAll("[$]", "").replaceAll("[?]", "").replaceAll("1\\s*:\\s*0",
+		// "").replaceAll(">=\\s*1", "");
 		String businessFormula = mathFormula.replaceAll("[$]|[?]|[1\\s*:\\s*0]|[>=\\s*1]", "");
-		int y=0;
+		int y = 0;
 	}
+	@Test
+	public void formula10() {
+		ExpressionParser parser = new SpelExpressionParser();
+		String mathFormula = "((9)>=7 && (1)==1 && (2)>=2)?1:0";
+		Assert.assertTrue(parser.parseExpression(mathFormula).getValue(String.class).equals("1"));
+		String businessFormula = mathFormula.replaceAll("([$])|(\\?\\s*[1]\\s*[:]\\s*[0])", "").replaceAll("(>\\s*=\\s*[1])", " >= 1 then 1 else 0");
+		Assert.assertTrue(businessFormula.equals("((9)>=7 && (1)==1 && (2)>=2)"));
+	}
+
+	@Test
+	public void formula9() {
+		ExpressionParser parser = new SpelExpressionParser();
+		String mathFormula = "(3)+(4)+ (((6<7) && (4>3))?2:1)";
+		Assert.assertTrue(parser.parseExpression(mathFormula).getValue(String.class).equals("9"));
+		// String businessFormula = mathFormula.replaceAll("[$]", "").replaceAll("[?]", "").replaceAll("1\\s*:\\s*0",
+		// "").replaceAll(">=\\s*1", "");
+		String businessFormula = mathFormula.replaceAll("[$]|[?]|[1\\s*:\\s*0]|[>=\\s*1]", "");
+		int y = 0;
+	}
+
 	@Test
 	public void formula8() {
 		ExpressionParser parser = new SpelExpressionParser();
 		String mathFormula = "([tbi_q1_score]+[tbi_q2_score]+[tbi_q3_score]+[tbi_q4_score]+[tbi_score])>=1?1:0";
 		String businessFormula = mathFormula.replaceAll("([$])|(\\?\\s*[1]\\s*[:]\\s*[0])", "").replaceAll("(>\\s*=\\s*[1])", " >= 1 then 1 else 0");
-		int y=0;
+		int y = 0;
 	}
+
 
 }
