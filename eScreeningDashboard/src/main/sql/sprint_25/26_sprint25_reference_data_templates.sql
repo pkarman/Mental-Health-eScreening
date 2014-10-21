7874fe12a33efcf68424a7db9198f390b8ac5566
iiiming <mzhu.ctr@iiinfo.com>
2014-10-21 14:17:44 -0400

    t680-fixed audit-c and advance directive

--- eScreeningDashboard/src/main/sql/sprint_25/26_sprint25_reference_data_templates.sql
+++ eScreeningDashboard/src/main/sql/sprint_25/26_sprint25_reference_data_templates.sql
@@ -446,3 +446,192 @@ ${MODULE_START}
 		${MATRIX_TABLE_END}
 ${MODULE_END}'
  WHERE template_id = 100;
+ 
+ -- /* ROAS AGGRESSION UPDATE */
+update template 
+set template_file = '
+<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+AGGRESSION: 
+${MODULE_TITLE_END}
+${MODULE_START}
+
+    	<#if (var3200.children)?? && (var3210.children)?? && (var3220.children)?? && (var3230.children)?? && (var3240.children)?? 
+				&& (var3250.children)?? && (var3260.children)?? && (var3270.children)?? && (var3280.children)?? && (var3290.children)?? 
+				&& (var3300.children)?? && (var3310.children)?? && (var3320.children)?? && (var3330.children)?? && (var3340.children)?? 
+				&& (var3350.children)??
+				&& ((var3200.children)?size > 0) && ((var3210.children)?size > 0) && ((var3220.children)?size > 0) && ((var3230.children)?size > 0) && ((var3240.children)?size > 0) 
+				&& ((var3250.children)?size > 0) && ((var3260.children)?size > 0) && ((var3270.children)?size > 0) && ((var3280.children)?size > 0) && ((var3290.children)?size > 0) 
+				&& ((var3300.children)?size > 0) && ((var3310.children)?size > 0) && ((var3320.children)?size > 0) && ((var3330.children)?size > 0) && ((var3340.children)?size > 0) 
+				&& ((var3350.children)?size > 0)>
+	
+			<#if !(getScore(var3200) == 999 ||  getScore(var3210) == 999 ||  getScore(var3220) == 999  || getScore(var3230) == 999  ||  getScore(var3240) == 999  || getScore(var3250) == 999  ||  getScore(var3260) == 999  
+				|| getScore(var3270) == 999  ||  getScore(var3280) == 999  || getScore(var3290) == 999)  ||  getScore(var3300) == 999 ||  getScore(var3310) == 999  || getScore(var3320) == 999  ||  getScore(var3330) == 999  
+				|| getScore(var3340) == 999  ||  getScore(var3350) == 999  >
+			
+			  <#if (var3389)?? && (getFormulaDisplayText(var3389) != "notfound") && (getFormulaDisplayText(var3389)?number == 0)>       <#-- veteran answered no to all questions" -->
+				${getAnsweredNeverAllText("Aggression")}.${NBSP}
+			  <#else>
+    			The Veteran had a score of ${getFormulaDisplayText(var3389)} on the Retrospective Overt Aggression Scale (range 0-240).${LINE_BREAK}${LINE_BREAK}
+    			
+    			<#assign fragments = []>
+	
+	
+				<#assign verbalAggressionText = "">
+				<#assign verbalAggressionSubText = []>
+				<#if (getQuestionCalcValue(var3200) > 1) >
+					<#assign verbalAggressionSubText = verbalAggressionSubText + ["shouting angrily"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3210) > 1) >
+					<#assign verbalAggressionSubText = verbalAggressionSubText + ["yelling mild insults"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3220) > 1) >
+					<#assign verbalAggressionSubText = verbalAggressionSubText + ["making vague threats"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3230) > 1) >
+					<#assign verbalAggressionSubText = verbalAggressionSubText + ["making clear threats of violence"]>
+				</#if>
+				<#if verbalAggressionSubText?has_content >      
+					<#assign verbalAggressionText = "Verbal Aggression (" + createSentence(verbalAggressionSubText) + ")">
+					<#assign fragments = fragments + [verbalAggressionText] >
+				</#if>
+
+				<#assign physicalAggressionObjectsText = "">
+				<#assign physicalAggressionObjectsSubText = []>
+				<#if (getQuestionCalcValue(var3240) > 1) >
+					<#assign physicalAggressionObjectsSubText = physicalAggressionObjectsSubText + ["slamming doors"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3250) > 1) >
+					<#assign physicalAggressionObjectsSubText = physicalAggressionObjectsSubText + ["throwing objects"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3260) > 1) >
+					<#assign physicalAggressionObjectsSubText = physicalAggressionObjectsSubText + ["breaking objects or smashing windows"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3270) > 1) >
+					<#assign physicalAggressionObjectsSubText = physicalAggressionObjectsSubText + ["setting fires or throwing object dangerously"]>
+				</#if>
+				<#if physicalAggressionObjectsSubText?has_content >      
+					<#assign physicalAggressionObjectsText = "Physical aggression toward objects (" + createSentence(physicalAggressionObjectsSubText) + ")">
+					<#assign fragments = fragments + [physicalAggressionObjectsText] >
+				</#if>
+
+
+
+				<#assign physicalAggressionSelfText = "">
+				<#assign physicalAggressionSelfSubText = []>
+				<#if (getQuestionCalcValue(var3320) > 1) >
+					<#assign physicalAggressionSelfSubText = physicalAggressionSelfSubText + ["hitting self or pulling own hair"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3330) > 1) >
+					<#assign physicalAggressionSelfSubText = physicalAggressionSelfSubText + ["banging head or fists against objects"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3340) > 1) >
+					<#assign physicalAggressionSelfSubText = physicalAggressionSelfSubText + ["cutting or bruising self"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3350) > 1) >
+					<#assign physicalAggressionSelfSubText = physicalAggressionSelfSubText + ["other serious self injury"]>
+				</#if>
+				<#if physicalAggressionSelfSubText?has_content >      
+					<#assign physicalAggressionSelfText = "Physical aggression toward self (" + createSentence(physicalAggressionSelfSubText) + ")">
+					<#assign fragments = fragments + [physicalAggressionSelfText] >
+				</#if>
+
+				<#assign physicalAggressionOthersText = "">
+				<#assign physicalAggressionOthersSubText = []>
+				<#if (getQuestionCalcValue(var3280) > 1) >
+					<#assign physicalAggressionOthersSubText = physicalAggressionOthersSubText + ["swinging at people or grabbing others"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3290) > 1) >
+					<#assign physicalAggressionOthersSubText = physicalAggressionOthersSubText + ["striking or pushing on others"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3300) > 1) >
+					<#assign physicalAggressionOthersSubText = physicalAggressionOthersSubText + ["bruising or causing welts on others"]>
+				</#if>
+				<#if (getQuestionCalcValue(var3310) > 1) >
+					<#assign physicalAggressionOthersSubText = physicalAggressionOthersSubText + ["attacking others causing severe physical injury"]>
+				</#if>
+				<#if physicalAggressionOthersSubText?has_content >      
+					<#assign physicalAggressionOthersText = "Physical aggression towards others (" + createSentence(physicalAggressionOthersSubText) + ")">
+					<#assign fragments = fragments + [physicalAggressionOthersText]  >
+				</#if>
+				The Veteran indicated the following concerning aggressive behaviors in the past month: ${createSentence(fragments)}.${NBSP}
+			  </#if>
+    		<#else>
+    			${getNotCompletedText()}
+    		</#if>
+    	<#else>
+    		${getNotCompletedText()}
+    	</#if>
+${MODULE_END}
+'
+where template_id = 38;
+
+UPDATE template SET template_file = 
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+ADVANCE DIRECTIVE:
+${MODULE_TITLE_END}
+${MODULE_START}
+  <#-- var800: ${var800}<br><br>var820: ${var820}<br><br> var826: ${var826}<br><br> --> <#-- test objs -->
+  <#assign nextLine = "">
+  	<#if (var800.children)?? && (var800.children?size > 0) || ((var820.children)??&& (var820.children?size > 0))
+  	 || ((var826.children)??  && (var826.children?size > 0))>
+    <#if (var800.children)?? && (var800.children?size > 0) && hasValue(getSelectMultiDisplayText(var800)) >
+    	<#-- In what language do you prefer to get your health information? -->
+      ${nextLine}The Veteran requests to receive information in ${getSelectMultiDisplayText(var800)}.
+      <#assign nextLine = "${LINE_BREAK}${LINE_BREAK}">
+    </#if>
+    <#if (var820.children)?? && (var820.children?size > 0) && hasValue(getSelectMultiDisplayText(var820)) >
+      <#-- Do you have and Advance Directive or Durable Power of Attorney for Healthcare?  -->
+      ${nextLine}The Veteran reported ${getSelectMultiDisplayText(var820)} an Advance Directive or Durable Power of Attorney for Healthcare.
+      <#assign nextLine = "${LINE_BREAK}${LINE_BREAK}">
+    <#else>
+     The Veteran declined to answer some or all of the questions.
+     </#if>
+    	   
+     <#if (var826.children)?? && (var826.children?size > 0) && hasValue(getSelectMultiDisplayText(var826)) >
+      	<#-- If no, would like information about this document? -->
+      	${nextLine}The Veteran ${getSelectMultiDisplayText(var826)} like information about an Advance Directive.
+    </#if>     
+    
+    <#else>
+    	${getNotCompletedText()}
+    </#if>    
+${MODULE_END}'
+where template_id = 10;
+
+UPDATE template SET template_file = 
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+ALCOHOL:
+${MODULE_TITLE_END}
+${MODULE_START}
+	<#if (var1200.children)?? && (var1210.children)?? && (var1220.children)??
+			&& ((var1200.children)?size > 0) && ((var1210.children)?size > 0) && ((var1220.children)?size > 0)>
+	
+	<#assign score = getListScore([var1200,var1210,var1220])>
+	<#assign status ="">  <#-- positive or negative -->
+	<#assign gender = "">
+	
+		<#if !((var1200.children)?has_content) || !((var1210.children)?has_content) || !((var1220.children)?has_content) >
+	 		${getNotCompletedText()}
+		<#else>
+			<#if (score >= 0) && (score <= 2)>
+				<#assign status = "negative">
+			<#elseif (score >= 4) && (score <= 998)>
+				<#assign status = "positive">
+			<#elseif (score == 3 )>
+				<#assign status = "positive for women/negative for men">
+			</#if>
+			The Veteran\'s AUDIT-C screen was ${status} with a score of ${score}. ${NBSP}
+    	</#if>
+    	
+    <#else>
+    	The Veteran\'s AUDIT-C screen was not calculated due to the Veteran declining to answer some or all of the questions.
+    </#if>
+${MODULE_END}
+'
+where template_id = 27;
