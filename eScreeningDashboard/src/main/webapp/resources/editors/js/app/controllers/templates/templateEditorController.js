ebbce7f4cce92411d736185acf78efd50a7817df
Robin Carnow <rcarnow@iiinfo.com>
2014-10-23 15:00:26 -0400

    #642 added all rules for drag-and-drop of blocks so we only allow what is required. Updated template to use the new summary block field.

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/templates/templateEditorController.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/templates/templateEditorController.js
@@ -5,6 +5,7 @@ Editors.controller('templateEditorController', ['$rootScope', '$scope', '$state'
     
     $scope.template = template;
     $scope.hasChanged = false;
+    $scope.debug = false;
     
     //TODO: change $stateParams to be more abstract (i.e. use relObj, relObjName, relObjType) so this can be reused for battery templates
     $scope.relatedObj = {
@@ -60,10 +61,138 @@ Editors.controller('templateEditorController', ['$rootScope', '$scope', '$state'
         $scope.templateChanged();
     };
     
+    function log(msg){
+        if($scope.debug)
+            console.log(msg);
+    }
+    
     //ng-tree options
     $scope.treeOptions = {
             dropped : function(event){
                 $scope.templateChanged();
+            },
+            accept: function(dragNodeScope, destNodesScope, destIndex){
+                log("*************");
+            	log("destIndex: " + destIndex);
+            	
+            	var destIsRoot = !Object.isDefined(destNodesScope.$nodeScope);
+            	var destParent = destIsRoot ? null : destNodesScope.$nodeScope.$modelValue;
+            	
+            	log("destParent type: " + (Object.isDefined(destParent) ? destParent.type : "null" ));
+            	
+            	var dragType = dragNodeScope.$modelValue.type;
+            	
+            	
+                if(!destIsRoot){
+                    log("destNodesScope.nodeScope: " + destNodesScope.$nodeScope.$modelValue.title);
+                }
+                
+                //allow dropping at the top of all blocks for text and if types
+                if(destIsRoot ){
+                    log("In root; if dragging text or if then drop, otherwise no drop");
+                    return dragType == "text" || dragType == "if";
+                }
+               
+                //text cannot have children 
+                if(!destIsRoot && destParent.type == "text"){
+                    log("No root and parent is text.  No drop");
+                    return false;
+                }
+                
+                //holds the first else or elseif index found
+                var firstElseIndex = Number.MAX_VALUE;
+                var firstElseIf = -1;
+                var lastElseIf = -1;
+                var elseIndex = -1;
+                var lastTextBlock = -1;
+                for(var i = 0; i < destNodesScope.$modelValue.length; i++){
+                    
+                    var type = destNodesScope.$modelValue[i].type;
+                    if(type == "elseif"){
+                        lastElseIf = i;
+                        if(firstElseIf == -1){
+                            firstElseIf = i;
+                        }
+                    }
+                    else if(type == "else" && elseIndex == -1){
+                        elseIndex = i;
+                    }
+                    else if(type == "text"){
+                        lastTextBlock = i;
+                    }
+                }
+                
+                //set firstElseIndex
+                if(firstElseIf > -1){
+                    firstElseIndex = firstElseIf;
+                }
+                if(elseIndex > -1){
+                    firstElseIndex = Math.min(firstElseIndex, elseIndex);
+                }
+                log("First else: " + elseIndex);
+                log("First elseif: " + firstElseIf);
+                log("Last elseif: " + lastElseIf);
+                log("First else or elseif: " + firstElseIndex);
+                log("Last text block: "+ lastTextBlock)
+                
+                //When text is being dropped it can only be dropped at: 
+                if(dragType == "text"){
+                    //1. as a child of an else
+                    //2. as a child of an elseif
+                    if(destParent.type == "else" || destParent.type == "elseif"){
+                        log("text is allowed to be dropped under else and elseif")
+                        return true;
+                    }
+                    //3. between an If and the first else or else if
+                    if(destParent.type == "if"){
+                        log("destIndex: " + destIndex + " <= firstElseIndex: " + firstElseIndex);
+                        //PLEASE NOTE: there seems to be a bug in ui-tree which gives the wrong index if 
+                        // you quickly move from the first node under the if to right under an elseif of the same if the index is still 1 which is incorrect
+                        return destIndex <= firstElseIndex;
+                    }
+                }
+                
+                //else and else if block cannot be dropped where the parent is not an if
+                if((dragType == "elseif" || dragType == "else") && destParent.type != "if"){
+                    log("else or elseif and parent is not if. No drop.");
+                    return false;
+                }
+                
+                //elseif cannot be dropped below an else
+                if(dragType == "elseif" && elseIndex != -1 && destIndex >= elseIndex){
+                    log("elseif cannot be dropped below an else");
+                    return false;
+                }
+                
+                //else cannot be dropped at an index less than the index of the last elseif
+                if(dragType == "else" && lastElseIf != -1 &&  destIndex <= lastElseIf){
+                    log("else cannot be dropped above an elseif");
+                    return false;
+                }
+                
+                //else and else if blocks cannot be dropped above an If's text entries (if text node has parent of if)
+                // or if dest is text and is sibling 
+                if((dragType == "elseif" || dragType == "else") && destIndex <= lastTextBlock){
+                    log("cannot drop else or elseif over an if's text blocks");
+                    return false;
+                }
+                
+                //else cannot be dropped where there exists another else
+                if(dragType == "else" && elseIndex != -1 && !destNodesScope.isParent(dragNodeScope)){
+                    log("cannot drop else from other if when the destination If has one already");
+                    return false;
+                }
+                
+                //PLEASE READ: dragging an else into another If which has an elseif as its last block is not 
+                //being allowed by ui-tree because it either:
+                // 1. if we drag a little higher, then this function is passed that we are trying to drop above the elseif (not allowed)
+                // 2. if we drag a little lower, then this function is passes a destNodesScope of the root block list (not allowed)
+                // So even though this operation should be allowed it is not at this time. The strange thing is that if the last 
+                // element of the If block is a text block, it will allow you to drop the else as a sibling
+                
+                
+                log("Allowed to drop here");
+                return true;
             },
     };
     
--- eScreeningDashboard/src/main/webapp/resources/editors/states.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/states.js
@@ -825,6 +825,7 @@ angular.module('Editors')
                                                          { 
                                                              section: "1.",
                                                              title: "depression_screening",
+                                                             summary: "Depression Screening: was calculated and has a score of:",
                                                              type: "text",
                                                              contents: [
 
@@ -850,6 +851,7 @@ angular.module('Editors')
                                                          {
                                                              section: "2.",
                                                              title: "if_dep_score_phq2",
+                                                             summary: "dep_score > 9",
                                                              type: "if",
                                                              left: 'var1599.value?number', 
                                                              operator: 'gt', 
@@ -876,20 +878,62 @@ angular.module('Editors')
                                                                         {
                                                                             section: "2.1",
                                                                             title: "Yes_NURSING",
+                                                                            summary: "Yes.  NURSING/NON-PROVIDER: Follow-up:",
                                                                             type: "text",
                                                                             content:  "${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}Yes.${LINE_BREAK} ${LINE_BREAK}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}NURSING/NON-PROVIDER: Follow-up:${LINE_BREAK}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}The following action was taken: Patient\'s provider,${LINE_BREAK}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}(Assigned Clincian), was notified for immediate intervention.${LINE_BREAK}",
                                                                             children: []
                                                                         },
-                                                                        
                                                                         {
                                                                             section: "2.2",
-                                                                            title: "else_block_1",
-                                                                            type: "else",
+                                                                            title: "else_if_block_1",
+                                                                            summary: "var1599 > 9",
+                                                                            type: "elseif",
+                                                                            left: 'var1599', 
+                                                                            operator: 'gt', 
+                                                                            right: 9,
                                                                             content:  "",
                                                                             children: [
                                                                                         {
                                                                                             section: "2.2.1",
-                                                                                            title: "No_1",
+                                                                                            summary: "No.",
+                                                                                            title: "else if text 1",
+                                                                                            type: "text",
+                                                                                            content: "${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}No.${LINE_BREAK}",
+                                                                                            children: []
+                                                                                        }
+                                                                                       ]
+                                                                        },
+                                                                        {
+                                                                            section: "2.3",
+                                                                            title: "else_if_block_2",
+                                                                            summary: "var1599 > 9",
+                                                                            type: "elseif",
+                                                                            left: 'var1599', 
+                                                                            operator: 'gt', 
+                                                                            right: 9,
+                                                                            content:  "",
+                                                                            children: [
+                                                                                        {
+                                                                                            section: "2.3.1",
+                                                                                            title: "else if text 2",
+                                                                                            summary: "else if: No.",
+                                                                                            type: "text",
+                                                                                            content: "${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}No.${LINE_BREAK}",
+                                                                                            children: []
+                                                                                        }
+                                                                                       ]
+                                                                        },
+                                                                        {
+                                                                            section: "2.4",
+                                                                            title: "else_block",
+                                                                            summary: "else",
+                                                                            type: "else",
+                                                                            content:  "",
+                                                                            children: [
+                                                                                        {
+                                                                                            section: "2.4.1",
+                                                                                            title: "else text block",
+                                                                                            summary: "else: No.",
                                                                                             type: "text",
                                                                                             content: "${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}No.${LINE_BREAK}",
                                                                                             children: []
@@ -897,7 +941,53 @@ angular.module('Editors')
                                                                                        ]
                                                                         }                                                              
                                                                        ]
-                                                         }                                                                                 
+                                                         },
+                                                         {
+                                                             section: "3.",
+                                                             title: "if_other",
+                                                             summary: "dep_score > 9",
+                                                             type: "if",
+                                                             left: 'var1599.value?number', 
+                                                             operator: 'gt', 
+                                                             right: 9,
+                                                             conditions: [
+                                                                { connector: 'and', 
+                                                                  left: 'var1599.value?number', 
+                                                                  operator: 'gt', 
+                                                                  right: 9,
+                                                                  conditions: [ 
+                                                                                {connector: 'and', 
+                                                                                 left: 'var1599.value?number', 
+                                                                                 operator: 'gt', 
+                                                                                 right: 9}
+                                                                              ]
+                                                                },
+                                                                { connector: 'or', 
+                                                                  left: 'var1599.value?number', 
+                                                                  operator: 'gt', 
+                                                                  right: 9
+                                                                }
+                                                             ],
+                                                             children: [
+                                                                {
+                                                                    section: "3.1",
+                                                                    title: "else_block",
+                                                                    summary: "else",
+                                                                    type: "else",
+                                                                    content:  "",
+                                                                    children: [
+                                                                                {
+                                                                                    section: "3.1.1",
+                                                                                    title: "else text block",
+                                                                                    summary: "else: No.",
+                                                                                    type: "text",
+                                                                                    content: "${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}${NBSP}No.${LINE_BREAK}",
+                                                                                    children: []
+                                                                                }
+                                                                               ]
+                                                                }     
+                                                              ]
+                                                         }
                                                         ];
                                         
                                         deferred.resolve(templateObj);
