9daca120696020dde8a1a5a66463fd5b0f782abf
kjsolution <liukai@gmail.com>
2014-10-23 21:25:06 -0700

    t642 add missing file.

---
+++ eScreeningDashboard/src/main/java/gov/va/escreening/dto/template/TemplateTextDTO.java
@@ -0,0 +1,42 @@
+package gov.va.escreening.dto.template;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class TemplateTextDTO extends TemplateBaseBlockDTO {
+	private List<TemplateBaseContent> contents;
+
+	public TemplateTextDTO() {
+		setType("text");
+	}
+
+	public List<TemplateBaseContent> getContents() {
+		return contents;
+	}
+
+	public void setContents(List<TemplateBaseContent> contents) {
+		this.contents = contents;
+	}
+
+	public String toFreeMarkerFormat() {
+		StringBuffer sb = new StringBuffer();
+
+		for (TemplateBaseContent content : contents) {
+			if(content instanceof TemplateTextContent)
+			{
+				sb.append(((TemplateTextContent)content).toFreeMarkerString());
+			}
+			else
+			{
+				// varirable content
+				sb.append("${"+((TemplateVariableContent)content).translate(null, content, null)+"}");
+			}
+		}
+
+		return sb.toString();
+	}
+
+}
