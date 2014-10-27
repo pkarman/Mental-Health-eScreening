a406772f2779969581179200a2b0f0ac9caaf160
Khalid R. Rizvi <javaguru786@gmail.com>
2014-10-27 14:01:58 -0400

    t692 fixed the declined issues

--- eScreeningDashboard/src/main/sql/sprint_25/26_sprint25_reference_data_templates.sql
+++ eScreeningDashboard/src/main/sql/sprint_25/26_sprint25_reference_data_templates.sql
@@ -12,7 +12,8 @@ ${MODULE_START}
 			<#assign scoreText = "positive">
 		</#if>
 	</#if>
-	<#if !var10714?? || (var10714?? && !10715??) || (var10715?? && !var10716??) || (var10716?? && !var10717??)>
+	<#if (var10714?? && var10714.value=="0" && var2016.value=="false") || (var10715?? && var10715.value=="0" && var2022.value=="false")
+		||(var10716?? && var10716.value=="0" && var2030.value=="false") || (var10717?? && var10717.value=="0" && var2037.value=="false") >
 		<#assign scoreText = "not calculated due to the Veteran declining to answer some or all of the questions">			
 	</#if>			
 	The Veteran\'s TBI screen was ${scoreText}. ${NBSP}
@@ -29,6 +30,11 @@ ${MODULE_START}
 ${MODULE_END}
 '
 where template_id = 30;
+
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2016, 30);
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2022, 30);
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2030, 30);
+INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2037, 30);
 
 /* ********************************************** */
 /* template   UPDATES */
@@ -636,31 +642,251 @@ ${MODULE_END}
 '
 where template_id = 27;
 
-update template set template_file = '<#include "clinicalnotefunctions"> <#if (var1599)?? > <#assign score = getCustomVariableDisplayText(var1599)> <#-- Template start --> ${MODULE_TITLE_START} Depression ${MODULE_TITLE_END} ${GRAPH_SECTION_START} ${GRAPH_BODY_START} {"type": "stacked", "title": "My Depression Score", "footer": "*a score of 10 or greater is a positive screen", "data": { "graphStart": 0,"ticks": [0, 4, 10, 15, 20, 27],"intervals": {"Minimal":4, "Mild":10, "Moderate":15, "Moderately Severe":20, "Severe":27 }, "score": ${score} } } ${GRAPH_BODY_END} ${GRAPH_SECTION_END}  ${MODULE_START} Depression is when you feel sad and hopeless for much of the time. It affects your body and thoughts, and interferes with daily life. There are effective treatments and resources for dealing with depression.${LINE_BREAK} <#if score?number == 999><b>Veteran declined to respond<b></#if><#if score?number != 999><b>Recommendation:</b> <#if score?number lt 10>Contact a clinician if in the future if you ever feel you may have a problem with depression</#if><#if score?number gt 9>Ask your clinician for further evaluation and treatment options</#if> ${MODULE_END}</#if></#if>' 
-where template_id=308;
+update template set template_file = 
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+Health Status:
+${MODULE_TITLE_END}
+${MODULE_START}
+	  <#if var1189?? && (var1020.children)?? && (var1030.children)?? && (var1040.children)?? && (var1050.children)?? && (var1060.children)?? 
+		&& (var1070.children)?? && (var1080.children)?? && (var1090.children)?? && (var1100.children)?? && (var1110.children)?? 
+		&& (var1120.children)?? && (var1130.children)?? && (var1140.children)?? && (var1150.children)?? && (var1160.children)??
+		&& ((var1020.children)?size > 0) && ((var1030.children)?size > 0) && ((var1040.children)?size > 0) && ((var1050.children)?size > 0) 
+		&& ((var1060.children)?size > 0) && ((var1070.children)?size > 0) && ((var1080.children)?size > 0) && ((var1090.children)?size > 0) 
+		&& ((var1100.children)?size > 0) && ((var1110.children)?size > 0) && ((var1120.children)?size > 0) && ((var1130.children)?size > 0) 
+		&& ((var1140.children)?size > 0) && ((var1150.children)?size > 0) && ((var1160.children)?size > 0)>  
+	
+	<#assign totalScore = getFormulaDisplayText(var1189)>
+	<#assign scoreText = "">
+	<#if totalScore != "notset" && totalScore != "notfound">
+		<#assign totalScore = totalScore?number>
+		<#if (totalScore <= 4)>
+			<#assign scoreText = "minimal">
+		<#elseif (totalScore >= 5 &&  (totalScore <= 9))>
+			<#assign scoreText = "low number of">
+		<#elseif (totalScore >= 10 &&  (totalScore <= 14))>
+			<#assign scoreText = "medium number of">
+		<#elseif (totalScore >= 15 &&  (totalScore <= 30))>
+			<#assign scoreText = "high number of">
+		</#if>
+	</#if>
 
-update template set template_file = '<#include "clinicalnotefunctions"> <#if var1929??> <#assign score = getCustomVariableDisplayText(var1929)>  <#-- Template start --> ${MODULE_TITLE_START} Post-traumatic Stress Disorder ${MODULE_TITLE_END}  ${GRAPH_SECTION_START}  ${GRAPH_BODY_START} { "type": "stacked", "title": "My PTSD Score", "footer": "", "data": { "graphStart": 17, "ticks": [17,35,50,65,85], "intervals": { "Negative": 49, "Positive":85 }, "score": ${score} } } ${GRAPH_BODY_END} ${GRAPH_SECTION_END}  ${MODULE_START} PTSD is when remembering a traumatic event keeps you from living a normal life. It\'s also called shell shock or combat stress. Common symptoms include recurring memories or nightmares of the event, sleeplessness, and feeling angry, irritable, or numb. ${LINE_BREAK} <#if score?number == 999><b>Veteran declined to respond<b></#if><#if score?number != 999><b>Recommendation:</b> <#if score?number gt 49>Ask your clinician for further evaluation and treatment options</#if><#if score?number lt 50>You may ask a clinician for help in the future if you feel you may have symptoms of PTSD</#if> ${MODULE_END} </#if></#if>' 
-where template_id=310;
+	<#-- During the past 4 weeks, how much have you been bothered by any of the following problems -->
+	<#-- this is almost identical to Other Health Symptoms -->
+	<#assign fragments = []> 
+    <#if (getScore(var1150) > 1)>
+		<#assign fragments = fragments + ["stomach pain"] >
+	</#if>
+    <#if (getScore(var1160) > 1)>
+		<#assign fragments = fragments + ["back pain"] >
+	</#if>
+	<#if (getScore(var1020) > 1)>
+		<#assign fragments = fragments + ["pain in arms, legs or joints (knees, hips, etc.)"] >
+	</#if>
+	<#if (getScore(var1030) > 1)>
+		<#assign fragments = fragments + ["menstrual cramps or other problems with your periods"] >
+	</#if>
+	<#if (getScore(var1040) > 1)>
+		<#assign fragments = fragments + ["headaches"] >
+	</#if>
+	<#if (getScore(var1050) > 1)>
+		<#assign fragments = fragments + ["chest pain"] >
+	</#if>
+	<#if (getScore(var1060) > 1)>
+		<#assign fragments = fragments + ["dizziness"] >
+	</#if>
+	<#if (getScore(var1070) > 1)>
+		<#assign fragments = fragments + ["fainting spells"] >
+	</#if>
+	<#if (getScore(var1080) > 1)>
+		<#assign fragments = fragments + ["feeling your heart pound or race"] >
+	</#if>
+	<#if (getScore(var1090) > 1)>
+		<#assign fragments = fragments + ["shortness of breath"] >
+	</#if>
+	<#if (getScore(var1100) > 1)>
+		<#assign fragments = fragments + ["pain or problems during sexual intercourse"] >
+	</#if>
+	<#if (getScore(var1110) > 1)>
+		<#assign fragments = fragments + ["constipation, loose bowels or diarrhea"] >
+	</#if>
+	<#if (getScore(var1120) > 1)>
+		<#assign fragments = fragments + ["nausea, gas or indigestion"] >
+	</#if>
+	<#if (getScore(var1130) > 1)>
+		<#assign fragments = fragments + ["feeling tired or having low energy"] >
+	</#if>
+	<#if (getScore(var1140) > 1)>
+		<#assign fragments = fragments + ["trouble sleeping"] >
+	</#if>
+
+	<#if fragments?has_content  >
+		<#assign resolved_fragments =  createSentence(fragments)>
+	<#else>
+		<#assign resolved_fragments = "None">
+	</#if>
+
+	The Veteran reported a ${scoreText} somatic symptoms.${LINE_BREAK}${LINE_BREAK}
+	The Veteran endorsed being bothered a lot by the following health symptoms over the past four weeks: ${resolved_fragments}.${NBSP}
+
+	
+	<#else>
+		${getNotCompletedText()}
+	</#if>
+${MODULE_END}'
+where template_id = 17;
 
 
+update template set template_file = 
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+PAIN:
+${MODULE_TITLE_END}
+${MODULE_START}
+	<#if (var2300.children)?? && (var2300.children?size>0) || (var2330.children)?? || (var2334?? && ((var2334.value) == "true"))>  
+			<#--  --> 
+			<#assign fragments = []>
+			<#assign text ="notset">  
+	 <#if (var2300.children)?? && (var2300.children?size>0)>
+			<#assign level = getSelectOneDisplayText(var2300)!("-1"?number)>
+			Over the past month, the Veteran\'s reported pain level was ${level} of 10.
+	 </#if>
+	 <#if var2330.children?? && (var2330.children?size>0) && var2334??>
+			<#assign bodyParts = getTableMeasureValueDisplayText(var2330)!"">
+			<#if !(bodyParts?has_content) && ((var2334.value) == "true")>
+				<#assign bodyParts = "none">
+			</#if>
+			
+			<#if bodyParts != "none">
+				The pain was located in: ${bodyParts}.
+			</#if>
+		</#if>
+	<#else>
+		${getNotCompletedText()}
+	</#if>
+${MODULE_END}
+'
+where template_id = 20;
 
+
+/*tobacco decline template */
+update template set template_file = '<#include "clinicalnotefunctions"> 
+<#-- Template start --> 
+${MODULE_TITLE_START} 
+	Tobacco Use 
+${MODULE_TITLE_END} 
+
+${MODULE_START}  
+	The use of tobacco causes harm to nearly every organ in the body. Quitting greatly lowers your risk of death from cancers, heart disease, stroke, and emphysema. There are many options, such as in-person and telephone counseling, nicotine replacement, and prescription medications. 
+
+	${LINE_BREAK} 
+	${LINE_BREAK} 
+
+	<b>Results:</b>${NBSP} 
+	
+	<#if (var600.children)?? && ((var600.children)?size gt 0)> 
+		<#if isSelectedAnswer(var600,var601) || isSelectedAnswer(var600,var602)> 
+			negative screen 
+		<#elseif isSelectedAnswer(var600,var603)> 
+			current user 
+			${LINE_BREAK} 
+			<b>Recommendations:</b> Prepare a plan to reduce or quit the use of tobacco. Get support from family and friends, and ask your clinician for help if needed. 
+		</#if> 
+	<#else>
+		<b>Veteran declined to respond</b>
+	</#if> 	
+${MODULE_END}' 
+where template_id=306;
+
+ 
 /*
 t692
 There is an issue with housing. The rule was: If HomelessCR_stable_house =1 then insert “stable housing”, if =0 “unstable housing/at risk”, if equal to 999 omit module. 
 However, this won’t work because the Veteran could have stable housing now and be worried about the future, which would mean they need assistance. 
 The changed rule is: 
-====>>> if HomelessCR_stable_worry =1 then insert “unstable housing/at risk”, if =0 “no housing concerns”, if equal to 999 insert “declined”.
+====>>> if HomelessCR_stable_worry =1 then insert “unstable housing/at risk”, if =0 “no housing concerns”, if equal to 999 insert “Veteran declined to respond”.
+====>>> also if user declined to respond then show Veteran declined to respond
 */
 update variable_template set assessment_variable_id=771 where variable_template_id=10511;
 update variable_template set assessment_variable_id=772 where variable_template_id=10512;
 update variable_template set assessment_variable_id=2001 where variable_template_id=10510;
 delete from variable_template where variable_template_id=10513;
 
-update template set template_file = '<#include "clinicalnotefunctions"> <#-- Template start --> <#if (var2001)?? && (var2001.children)?? && ((var2001.children)?size > 0)> ${MODULE_TITLE_START} Homelessness ${MODULE_TITLE_END} ${MODULE_START} This is when you do not have a safe or stable place you can return to every night. The VA is committed to ending Veteran homelessness by the end of 2015. ${LINE_BREAK} ${LINE_BREAK} <b>Results:</b>${NBSP} <#if isSelectedAnswer(var2001,var772)> unstable housing/at risk ${LINE_BREAK} <b>Recommendation:</b> Call the VA\'s free National Call Center for Homeless Veterans at (877)-424-3838 and ask for help. Someone is always there to take your call. <#elseif isSelectedAnswer(var2001,var771)> stable housing </#if>  ${MODULE_END} </#if>' 
+update template set template_file = '<#include "clinicalnotefunctions"> 
+<#-- Template start --> 
+${MODULE_TITLE_START} Homelessness ${MODULE_TITLE_END} 
+${MODULE_START} This is when you do not have a safe or stable place you can return to every night. The VA is committed to ending Veteran homelessness by the end of 2015. ${LINE_BREAK} 
+${LINE_BREAK} 
+<b>Results:</b>${NBSP} 
+<#if (var2001)?? && (var2001.children)?? && ((var2001.children)?size > 0)> 
+	<#if isSelectedAnswer(var2001,var772)> 
+		unstable housing/at risk 
+		${LINE_BREAK} 
+		<b>Recommendation:</b> Call the VA\'s free National Call Center for Homeless Veterans at (877)-424-3838 and ask for help. Someone is always there to take your call. 
+	<#elseif isSelectedAnswer(var2001,var771)> 
+		stable housing 
+	</#if>  
+<#else>
+	<b>Veteran declined to respond</b>
+</#if>
+${MODULE_END}' 
 where template_id=301;
 
-/*
-update template set template_file = '<#include "clinicalnotefunctions"> <#-- Template start --> ${MODULE_TITLE_START} Tobacco Use ${MODULE_TITLE_END} ${MODULE_START} The use of tobacco causes harm to nearly every organ in the body. Quitting greatly lowers your risk of death from cancers, heart disease, stroke, and emphysema. There are many options, such as in-person and telephone counseling, nicotine replacement, and prescription medications. ${LINE_BREAK} ${LINE_BREAK} <b> Results:</b> ${NBSP} <#if (var2000)?? && (var2000.children)?? && ((var2000.children)?size > 0)> <#if isSelectedAnswer(var600,var601) || isSelectedAnswer(var600,var602)> negative screen <#elseif isSelectedAnswer(var600,var603)> current user ${LINE_BREAK} <b> Recommendations: Prepare a plan to reduce or quit the use of tobacco. Get support from family and friends, and ask your clinician for help if needed. </#if> <#else><b> Declined<b> </#if> ${MODULE_END}' 
-where template_id=306;
-*/
+ 
+/* depression declined template */
+update template set template_file = '<#include "clinicalnotefunctions"> 
+<#assign score = getCustomVariableDisplayText(var1599)> 
+<#-- Template start --> 
+${MODULE_TITLE_START} Depression ${MODULE_TITLE_END} 
+<#if score != "notfound">
+	${GRAPH_SECTION_START} 
+	${GRAPH_BODY_START} 
+		{"type": "stacked", "title": "My Depression Score", "footer": "*a score of 10 or greater is a positive screen", "data": { "graphStart": 0,"ticks": [0, 4, 10, 15, 20, 27],"intervals": {"Minimal":4, "Mild":10, "Moderate":15, "Moderately Severe":20, "Severe":27 }, "score": ${score} } } 
+	${GRAPH_BODY_END} 
+	${GRAPH_SECTION_END}  
+</#if>
+${MODULE_START} 
+	Depression is when you feel sad and hopeless for much of the time. It affects your body and thoughts, and interferes with daily life. There are effective treatments and resources for dealing with depression.
+	${LINE_BREAK} 
+	<b>Recommendation:</b> 
+	<#if score = "notfound">
+		<b>Veteran declined to respond</b>
+	<#elseif score?number lt 10>
+		Contact a clinician if in the future if you ever feel you may have a problem with depression
+	<#elseif score?number gt 9>
+		Ask your clinician for further evaluation and treatment options
+	</#if> 
+${MODULE_END}' 
+where template_id=308;
+
+/* PTSD declined template */
+update template set template_file = '<#include "clinicalnotefunctions"> 
+<#assign score = getCustomVariableDisplayText(var1929)>  
+<#-- Template start --> 
+${MODULE_TITLE_START} Post-traumatic Stress Disorder ${MODULE_TITLE_END}  
+
+<#if score != "notfound">
+	${GRAPH_SECTION_START}  
+	${GRAPH_BODY_START} 
+	{ "type": "stacked", "title": "My PTSD Score", "footer": "", "data": { "graphStart": 17, "ticks": [17,35,50,65,85], "intervals": { "Negative": 49, "Positive":85 }, "score": ${score} } } 
+	${GRAPH_BODY_END} 
+	${GRAPH_SECTION_END}  
+</#if>
+
+${MODULE_START} 
+	PTSD is when remembering a traumatic event keeps you from living a normal life. It\'s also called shell shock or combat stress. Common symptoms include recurring memories or nightmares of the event, sleeplessness, and feeling angry, irresistible, or numb. 
+	${LINE_BREAK} 
+	<b>Recommendation:</b> 
+	<#if score = "notfound">
+		<b>Veteran declined to respond</b>
+	<#elseif score?number gt 49>
+		Ask your clinician for further evaluation and treatment options
+	<#elseif score?number lt 50>
+		You may ask a clinician for help in the future if you feel you may have symptoms of PTSD
+	</#if> 
+${MODULE_END}' 
+where template_id=310;
+
  
