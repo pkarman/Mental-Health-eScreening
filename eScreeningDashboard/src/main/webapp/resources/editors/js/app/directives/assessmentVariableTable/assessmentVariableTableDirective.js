96360594ce0021a413903e491cb9fdab0e1de120
Tonté Pouncil <pouncilt.developer@gmail.com>
2014-10-30 18:19:03 -0500

    #661 - Removed Answer from filter.

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/directives/assessmentVariableTable/assessmentVariableTableDirective.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/directives/assessmentVariableTable/assessmentVariableTableDirective.js
@@ -15,7 +15,7 @@
                 data: '=assessmentVariables'
             },
             controller: ['$scope', '$rootScope', '$filter', 'ngTableParams', 'AssessmentVariableService', function ($scope, $rootScope, $filter, ngTableParams, AssessmentVariableService) {
-                $scope.assessmentVariableTypes = ['Question', 'Answer', 'Custom', 'Formula'];
+                $scope.assessmentVariableTypes = ['Question', 'Custom', 'Formula'];
 
                 $scope.selectAssessmentVariable = function(someAssessmentVariable) {
 	                // This is needed to trigger a change on $scope.$watch
