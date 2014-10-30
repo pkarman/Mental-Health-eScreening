1b0195e5e28c1183e5a4160f111a1ff0adad545e
Robin Carnow <rcarnow@iiinfo.com>
2014-10-30 17:00:24 -0400

    #642 removed example template from DB scripts as well as a test that tested viasta question and answer templates

--- eScreeningDashboard/src/main/sql/ticketRelatedChanges/27_sprintXX_reference_data_template_variables.sql
+++ eScreeningDashboard/src/main/sql/ticketRelatedChanges/27_sprintXX_reference_data_template_variables.sql
@@ -6,17 +6,5 @@
 UPDATE assessment_variable SET display_name='dep_score_phq9', description='PHQ-9 Score' 
 WHERE assessment_variable_id=1599;
 
--- associate new template with the variables it uses
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2960, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2970, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2980, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (2990, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (3000, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (1550, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (1560, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (1570, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (1580, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (1590, 311);
-INSERT INTO variable_template(assessment_variable_id, template_id) VALUES (1599, 311);
 
 
