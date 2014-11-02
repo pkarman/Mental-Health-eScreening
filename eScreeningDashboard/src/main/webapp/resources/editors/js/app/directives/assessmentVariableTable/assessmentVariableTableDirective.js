84bdd2d5d928e47e8ed86033d24a2da084fac5da
Tonté Pouncil <pouncilt.developer@gmail.com>
2014-11-02 17:39:03 -0600

    #661 - Got pre-populating the assessment drop down menus to work.

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/directives/assessmentVariableTable/assessmentVariableTableDirective.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/directives/assessmentVariableTable/assessmentVariableTableDirective.js
@@ -14,7 +14,7 @@
                 surveyId: '@',
                 data: '=assessmentVariables'
             },
-            controller: ['$scope', '$rootScope', '$filter', 'ngTableParams', 'AssessmentVariableService', function ($scope, $rootScope, $filter, ngTableParams, AssessmentVariableService) {
+            controller: ['$scope', '$rootScope', '$filter', 'ngTableParams', 'AssessmentVariableService', 'limitToWithEllipsisFilter', function ($scope, $rootScope, $filter, ngTableParams, AssessmentVariableService, limitToWithEllipsisFilter) {
                 $scope.assessmentVariableTypes = ['Question', 'Custom', 'Formula'];
 
                 $scope.selectAssessmentVariable = function(someAssessmentVariable) {
@@ -52,6 +52,10 @@
                         counts: [],
                         total: 0,
                         getData: function ($defer, params) {
+                            if(Object.isDefined($scope.assessmentVariable) && Object.isDefined($scope.assessmentVariable.id)) {
+                                $(".assessmentVariableSelection[guid=\""+$scope.guid+"\"]").find("#assessmentVariableMenuLabel").text(" " + limitToWithEllipsisFilter($scope.assessmentVariable.name, 20));
+                                $scope.$emit('filterOperators', {guid: $scope.guid, selectedAssessmentVariable: $scope.assessmentVariable});
+                            }
                             if(Object.isDefined($scope.data)) {
                                 $scope.data.then(function (assessmentVariables) {
                                     params.total(assessmentVariables.length);
