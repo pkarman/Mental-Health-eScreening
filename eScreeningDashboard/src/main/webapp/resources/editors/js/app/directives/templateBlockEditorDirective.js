85d6598f7b17382f36627c2e6b3d0c6c1dba5aef
Aaron Roberson <aaronaroberson@gmail.com>
2014-11-05 08:28:38 -0800

    #661 Fixed error in form validation expression

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/directives/templateBlockEditorDirective.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/directives/templateBlockEditorDirective.js
@@ -80,8 +80,6 @@
             templateUrl: 'resources/editors/views/templates/templateblockeditor.html',
             link: function(scope, element, attrs, formController) {
 
-	            console.log(scope.showValidationMessages);
-
                 /* Temporarily disabled until further notice: 11/03/14
                  var collectionTemplate = '<template-block-editor block="member" ng-repeat="member in block.children | limitTo:2" assessment-variables="assessmentVariables"></template-block-editor>';
                   */
