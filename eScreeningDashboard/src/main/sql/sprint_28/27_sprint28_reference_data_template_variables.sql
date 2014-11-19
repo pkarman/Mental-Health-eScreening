19112ec8d818bd8d06e8ee62171b8c5e1f27d2aa
Khalid R. Rizvi <javaguru786@gmail.com>
2014-11-19 13:25:29 -0500

    t723 changed from serv_oef to serv_oef_oif

---
+++ eScreeningDashboard/src/main/sql/sprint_28/27_sprint28_reference_data_template_variables.sql
@@ -0,0 +1,3 @@
+UPDATE `escreening`.`measure_answer` SET `export_name`='serv_oef_oif' WHERE `measure_answer_id`='1120';
+UPDATE `escreening`.`measure_answer` SET `export_name`='serv_oef_oif' WHERE `measure_answer_id`='1121';
+UPDATE `escreening`.`measure_answer` SET `export_name`='serv_oef_oif' WHERE `measure_answer_id`='1122';
