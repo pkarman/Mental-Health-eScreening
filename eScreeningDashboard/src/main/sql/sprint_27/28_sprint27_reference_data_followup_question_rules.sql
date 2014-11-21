cc38fdd3bc939b911c544ffcd859cd0ed70adbfe
iiiming <mzhu.ctr@iiinfo.com>
2014-11-21 16:16:33 -0500

    t726-- fixed the MST followup logic

---
+++ eScreeningDashboard/src/main/sql/sprint_27/28_sprint27_reference_data_followup_question_rules.sql
@@ -0,0 +1,14 @@
+/* *********************************************************************/
+/* Ticket 726-- Refine MST followup logic */
+/* *********************************************************************/
+
+delete from rule_event where event_id=60;
+delete from event where event_id=60;
+
+
+
+
+
+
+
+
