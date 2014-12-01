150b31b473988319ea3dda7a305c976f5d2d2d24
Robin Carnow <rcarnow@iiinfo.com>
2014-12-01 13:36:51 -0500

    #660 added Kai's remove duplicate variable_template entry script

--- eScreeningDashboard/src/main/sql/sprint_25/27_sprint25_reference_data_template_variables.sql
+++ eScreeningDashboard/src/main/sql/sprint_25/27_sprint25_reference_data_template_variables.sql
@@ -7,6 +7,14 @@ UPDATE assessment_variable SET display_name='dep_score_phq9', description='PHQ-9
 WHERE assessment_variable_id=1599;
 
 
+/* Removing duplicate entries in the variable_template table (found by Ming) */
+delete from variable_template
+where variable_template_id not in (
+select cid from (
+    (select max(variable_template_id) as cid from variable_template b
+        group by template_id, assessment_variable_id)
+    ) as C
+);
 
 /*
 update statements to sync assessment variables' display names + description with measure answer
