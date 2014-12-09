36ab300767547b7be98e37ace5712c173008dc82
Khalid R. Rizvi <javaguru786@gmail.com>
2014-12-08 21:05:43 -0500

    t740 added an option of None for Service History

---
+++ eScreeningDashboard/src/main/sql/sprint_28/25_sprint28_reference_data_modules.sql
@@ -0,0 +1,3 @@
+/* following insert script is added to fix t740 */
+INSERT INTO measure_answer (measure_id, export_name, answer_text, answer_type, answer_value, display_order)
+VALUES ('126', 'serv_exp_none', 'None', 'none', '0', '0');
