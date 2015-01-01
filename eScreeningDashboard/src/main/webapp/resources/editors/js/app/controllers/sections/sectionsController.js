df757ed8ad0c31af27c6099cc0c2d8818cb2ff6b
Khalid R. Rizvi <javaguru786@gmail.com>
2014-12-31 20:00:55 -0500

    t651 fixed json data response by plucking unncessary data and also Lint the section controller

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/sections/sectionsController.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/sections/sectionsController.js
@@ -1,103 +1,41 @@
 Editors.controller('sectionsController', ['$log', '$scope', '$state', 'ManageSectionService', function ($log, $scope, $state, ManageSectionService) {
-    var toBeDel = {sections: []};
-    var dbData = [];
-
-    $scope.msgs = [];
-    $scope.status = {
-        closeOthers: true,
-        dd: {
-            sections: {
-                selected: null
-            },
-            modules: {
-                selected: null
+    'use strict';
+    var toBeDel = {sections: []},
+        dbData = [],
+        clearMsgs = function () {
+            $scope.msgs = [];
+        },
+        addMsg = function (reset, type, msg) {
+            if (reset) {
+                clearMsgs();
             }
-        }
-    };
-    $scope.ssRows = [];
-
-    var addSuccessMsg = function (reset, reason) {
-        addMsg(reset, 'success', reason);
-    };
-
-    var clearMsgs = function () {
-        $scope.msgs = [];
-    };
-
-    var addMsg = function (reset, type, msg) {
-        if (reset) {
-            clearMsgs();
-        }
-        $scope.msgs.push({type: type, text: msg});
-    };
-
-    var addDangerMsg = function (reset, reason) {
-        addMsg(reset, 'danger', reason);
-    };
-
-    $scope.closeAlert = function (index) {
-        $scope.msgs.splice(index, 1);
-    };
-
-    $scope.openAll = function () {
-        $scope.status.closeOthers = !$scope.status.closeOthers;
-        _.each($scope.ssRows, function (ssRow) {
-            ssRow.openSection = !$scope.status.closeOthers;
-        })
-    }
-
-    $scope.addSection = function () {
-        $scope.ssRows.unshift({id: null, name: '', description: '', displayOrder: 0, surveys: []});
-        addSuccessMsg(true, 'Please enter the name and description of new survey section');
-    };
-
-    var refreshNow = function () {
-        ManageSectionService.getList()
-            .then(function (fetchedDataFromBackend) {
-                dbData = fetchedDataFromBackend;
-                $scope.ssRows = updateScopeDataWithDbData(fetchedDataFromBackend);
-            }, function error(reason) {
-                addDangerMsg(false, reason);
+            $scope.msgs.push({type: type, text: msg});
+        },
+        addSuccessMsg = function (reset, reason) {
+            addMsg(reset, 'success', reason);
+        },
+        addDangerMsg = function (reset, reason) {
+            addMsg(reset, 'danger', reason);
+        },
+        updateScopeDataWithDbData = function (backEndData) {
+            var recs = [];
+            _.each(backEndData, function (singleRecord) {
+                var rec = _.pick(singleRecord, ['id', 'name', 'description', 'displayOrder', 'surveys']);
+                rec.openSection = false;
+                rec.openModule = false;
+                recs.push(rec);
             });
-    };
-
-    var updateScopeDataWithDbData = function (backEndData) {
-        var recs = [];
-        _.each(backEndData, function (singleRecord) {
-            var rec = _.pick(singleRecord, ['id', 'name', 'description', 'displayOrder', 'surveys']);
-            rec.openSection = false;
-            rec.openModule = false;
-            recs.push(rec);
-        });
-        return recs;
-    }
-
-    $scope.cancel = function () {
-        $state.go("home");
-    }
-
-    $scope.saveAll = function () {
-        saveAll();
-    };
-
-    $scope.delete = function (index) {
-        var section = $scope.ssRows[index];
-
-        if (section.surveys !== undefined && section.surveys.length > 0) {
-            addDangerMsg(true, '\'' + section.name + '\' has [' + section.surveys.length + '] module(s) and cannot be removed');
-        } else if (section.id == null) {
-            $scope.ssRows.splice(index, 1);
-            addSuccessMsg(true, 'Newly added Survey Section \'' + (section.name || 'No Name provided') + '\' has been deleted');
-        } else {
-            // transfer the information of ssRow to dbData by finding the correct dbData.row based on the ssRow id
-            toBeDel.sections.push(_.where(dbData, {id: section.id})[0]);
-
-            // remove the section from $scope.ssRows but DO NOT save it the db
-            //$scope.ssRows.splice(index, 1);
-
-            addSuccessMsg(true, 'Survey Section \'' + section.name + '\' is marked for deletion. Please press \'Save\' to delete the section permanently');
-        }
-    };
+            return recs;
+        },
+        refreshNow = function () {
+            ManageSectionService.getList()
+                .then(function (fetchedDataFromBackend) {
+                    dbData = fetchedDataFromBackend;
+                    $scope.ssRows = updateScopeDataWithDbData(fetchedDataFromBackend);
+                }, function error(reason) {
+                    addDangerMsg(false, reason);
+                });
+        };
 
     function adjustDisplayOrders() {
         //reset the display Order as this could be out of order when
@@ -120,7 +58,6 @@ Editors.controller('sectionsController', ['$log', '$scope', '$state', 'ManageSec
                 tmp.push(ss);
             }
         });
-
         dbData = dbData.concat(tmp);
     }
 
@@ -132,10 +69,10 @@ Editors.controller('sectionsController', ['$log', '$scope', '$state', 'ManageSec
             addDangerMsg(true, 'Section must have a name');
         }
 
-        return errSectionName == undefined;
+        return errSectionName === undefined;
     }
 
-    function saveAll(foo) {
+    function saveAll() {
         if (passBasicSanityChecks()) {
 
             adjustDisplayOrders();
@@ -148,5 +85,57 @@ Editors.controller('sectionsController', ['$log', '$scope', '$state', 'ManageSec
 
     //refresh the view on entry
     refreshNow();
+
+    $scope.msgs = [];
+    $scope.status = {
+        closeOthers: true,
+        dd: {
+            sections: {
+                selected: null
+            },
+            modules: {
+                selected: null
+            }
+        }
+    };
+    $scope.ssRows = [];
+    $scope.closeAlert = function (index) {
+        $scope.msgs.splice(index, 1);
+    };
+    $scope.openAll = function () {
+        $scope.status.closeOthers = !$scope.status.closeOthers;
+        _.each($scope.ssRows, function (ssRow) {
+            ssRow.openSection = !$scope.status.closeOthers;
+        });
+    };
+    $scope.addSection = function () {
+        $scope.ssRows.unshift({id: null, name: '', description: '', displayOrder: 0, surveys: []});
+        addSuccessMsg(true, 'Please enter the name and description of new survey section');
+    };
+    $scope.cancel = function () {
+        $state.go("home");
+    };
+    $scope.saveAll = function () {
+        saveAll();
+    };
+    $scope.delete = function (index) {
+        var section = $scope.ssRows[index];
+
+        if (section.surveys !== undefined && section.surveys.length > 0) {
+            addDangerMsg(true, '\'' + section.name + '\' has [' + section.surveys.length + '] module(s) and cannot be removed');
+        } else if (section.id === null) {
+            $scope.ssRows.splice(index, 1);
+            addSuccessMsg(true, 'Newly added Survey Section \'' + (section.name || 'No Name provided') + '\' has been deleted');
+        } else {
+            // transfer the information of ssRow to dbData by finding the correct dbData.row based on the ssRow id
+            toBeDel.sections.push(_.where(dbData, {id: section.id})[0]);
+
+            // remove the section from $scope.ssRows but DO NOT save it the db
+            //$scope.ssRows.splice(index, 1);
+
+            addSuccessMsg(true, 'Survey Section \'' + section.name + '\' is marked for deletion. Please press \'Save\' to delete the section permanently');
+        }
+    };
+
 
 }]);
