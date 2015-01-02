97283873079e6f2a10b943e3e677d9319c889fcc
Aaron Roberson <aaronaroberson@gmail.com>
2015-01-02 07:59:10 -0800

    t693 Updated alerts to use angular-ui-bootstrap alerts

--- eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/modules/modules.detail.controller.js
+++ eScreeningDashboard/src/main/webapp/resources/editors/js/app/controllers/modules/modules.detail.controller.js
@@ -13,6 +13,16 @@
             });
         }
 
+        $scope.alerts = [];
+
+        $scope.addAlert = function() {
+            $scope.alerts.push({msg: 'Another alert!'});
+        };
+
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+
         $scope.sortablePageOptions = {
             'ui-floating': false,
             cancel: '.unsortable',
