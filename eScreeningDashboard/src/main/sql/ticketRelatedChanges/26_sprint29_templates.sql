b98b92c9f99ab460aa66640c6d6226cc922de6d3
Robin Carnow <rcarnow@iiinfo.com>
2015-01-09 10:14:49 -0500

    #760 Removed extra word 'pain' in Infect and Embedded cprs module tempalte

--- eScreeningDashboard/src/main/sql/ticketRelatedChanges/26_sprint29_templates.sql
+++ eScreeningDashboard/src/main/sql/ticketRelatedChanges/26_sprint29_templates.sql
@@ -419,3 +419,44 @@ ${MODULE_START}
 ${MODULE_END}'
 where template_id = 16;
 
+-- fix extra "pain" word stuck in for no reason (for ticket 760)
+update template set template_file = 
+'<#include "clinicalnotefunctions"> 
+<#-- Template start -->
+${MODULE_TITLE_START}
+Screen for Infectious Disease and Embedded Fragments:
+${MODULE_TITLE_END}
+${MODULE_START}
+	<#if (var2500.children)?? && ((var2500.children)?size > 0) || ((var2501.children)?? && ((var2501.children)?size > 0))
+	 || ((var2502.children)?? && ((var2502.children)?size > 0)) || ((var2009.children)?? && ((var2009.children)?size > 0))>
+
+	<#-- ${createSentence(parts)}. -->
+	<#assign questions = [var2500,var2501,var2502,var2009]>
+	<#assign textHash = {"var2500": "chronic diarrhea or other gastrointestinal", 
+							"var2501": "unexplained fevers", 
+							"var2502": "persistent papular or nodular skin rash that began after deployment to Southwest Asia", 
+							"var2009": "suspects that he/she has retained fragments or shrapnel as a result of injuries"}>
+	
+	<#assign parts = []>
+		
+	<#list questions as q >
+		<#if q.children?? && ((q.children)?size>0)>
+			<#assign child = q.children[0] >
+			<#if ((child.calculationValue?number) > 0)>
+				<#assign problem = textHash[q.key] >
+				<#assign part = problem + ", with symptoms of " + getVariableDisplayText(child) >
+				<#assign parts = parts + [part] >
+			</#if>
+		</#if>
+	</#list>
+	
+	<#if parts?has_content>
+		The Veteran endorsed other health problems of: ${createSentence(parts)}. ${NBSP}
+	<#else>
+		${getAnsweredNoAllText("OOO Infect & Embedded Fragment CR")}. ${NBSP}
+	</#if>
+	<#else>
+		${getNotCompletedText()}
+	</#if>
+${MODULE_END}'
+where template_id = 19;
