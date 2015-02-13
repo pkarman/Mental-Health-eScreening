d4fdb53d4275d2003922fde99c2db0e00cf11fc4
Khalid R. Rizvi <javaguru786@gmail.com>
2015-02-13 15:44:55 -0500

    t801 fixed teh dast scoring issue by manipulating dast_score formula

---
+++ eScreeningDashboard/src/main/sql/sprint_32/27_sprint_32_reference_data_template_variables.sql
@@ -0,0 +1,4 @@
+UPDATE assessment_variable
+SET
+  formula_template = '([1000] + [1001] + [1003] + [1004] + [1005] + [1006] + [1007] + [1008] + [1009] + T(Math).min([1000],(1 - [1002])))'
+WHERE assessment_variable_id = 1010;
