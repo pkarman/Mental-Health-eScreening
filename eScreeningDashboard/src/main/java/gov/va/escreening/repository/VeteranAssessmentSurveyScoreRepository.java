e899f12da337f534982286d05bf1b4ecdcb3e981
kjsolution <liukai@gmail.com>
2015-03-05 23:18:50 -0800

    new methods to retrieve data from score table.

--- eScreeningDashboard/src/main/java/gov/va/escreening/repository/VeteranAssessmentSurveyScoreRepository.java
+++ eScreeningDashboard/src/main/java/gov/va/escreening/repository/VeteranAssessmentSurveyScoreRepository.java
@@ -3,8 +3,16 @@ package gov.va.escreening.repository;
 import gov.va.escreening.entity.VeteranAssessmentSurvey;
 import gov.va.escreening.entity.VeteranAssessmentSurveyScore;
 
+import java.util.List;
+
 /**
  * Created by kliu on 3/3/15.
  */
-public interface VeteranAssessmentSurveyScoreRepository  extends RepositoryInterface<VeteranAssessmentSurveyScore> {
+public interface VeteranAssessmentSurveyScoreRepository extends RepositoryInterface<VeteranAssessmentSurveyScore> {
+    public List<VeteranAssessmentSurveyScore> getDataForIndividual(String lastName, String ssnLastFour, List<Integer> surveyIds, String fromDate, String toDate);
+
+    public List<VeteranAssessmentSurveyScore> getIndividualDataForClicnic(List<Integer> clinicIds, List<Integer> surveyIds,
+                                                                          String fromDate, String toDate);
+    public List<VeteranAssessmentSurveyScore> getDataForClicnic(List<Integer> clinicIds, List<Integer> surveyIds,
+                                                                String fromDate, String toDate);
 }
--- eScreeningDashboard/src/main/java/gov/va/escreening/repository/VeteranAssessmentSurveyScoreRepositoryImpl.java
+++ eScreeningDashboard/src/main/java/gov/va/escreening/repository/VeteranAssessmentSurveyScoreRepositoryImpl.java
@@ -2,10 +2,13 @@ package gov.va.escreening.repository;
 
 import gov.va.escreening.entity.Survey;
 import gov.va.escreening.entity.Veteran;
+import gov.va.escreening.entity.VeteranAssessmentSurvey;
 import gov.va.escreening.entity.VeteranAssessmentSurveyScore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.TypedQuery;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,42 +28,81 @@ public class VeteranAssessmentSurveyScoreRepositoryImpl extends AbstractHibernat
         setClazz(VeteranAssessmentSurveyScore.class);
     }
 
-    public List<VeteranAssessmentSurveyScore> getDataForIndividual(String lastName, String ssnLastFour, List<Survey> surveys, String fromDate, String toDate) {
+    @Override
+    public List<VeteranAssessmentSurveyScore> getDataForIndividual(String lastName, String ssnLastFour, List<Integer> surveyIds, String fromDate, String toDate) {
 
         List<VeteranAssessmentSurveyScore> scores = new ArrayList<>();
 
         List<Veteran> veterans = veteranRepository.searchVeterans(lastName, ssnLastFour);
 
+
+
         if (veterans != null && veterans.size() > 0) {
 
+            List<Integer> veteranIds = new ArrayList<>(veterans.size());
 
-            String query = "select vassr from VeteranAssessmentSurveyScore vassr  " +
+            for(Veteran v : veterans){
+                veteranIds.add(v.getVeteranId());
+            }
 
+            String hql = "select vassr from VeteranAssessmentSurveyScore vassr  " +
                     " where vassr.dateCompleted >= :fromDate " +
                     " and vassr.dateCompeted <= :toDate " +
                     " and vassr.survey.id in ( :surveyIds ) " +
                     " and vassr.veteran.id in ( :veteranIds )" +
                     " order by vassr.survey.id, vassr.dateCompleted desc ";
 
+            TypedQuery<VeteranAssessmentSurveyScore> query = entityManager.createQuery(hql, VeteranAssessmentSurveyScore.class);
+            query.setParameter("surveyIds", surveyIds);
+            query.setParameter("veteranIds", veteranIds);
+            query.setParameter("fromDate", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(fromDate+" 00:00:00"));
+            query.setParameter("toDate", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(toDate+" 23:59:59"));
+
+            scores = query.getResultList();
+
         }
         return scores;
     }
 
-    public List<VeteranAssessmentSurveyScore> getDataForClicnic(List<Integer> clinicIds, List<Integer> surveyIds,
+    @Override
+    public List<VeteranAssessmentSurveyScore> getIndividualDataForClicnic(List<Integer> clinicIds, List<Integer> surveyIds,
                                                                 String fromDate, String toDate) {
 
-        List<VeteranAssessmentSurveyScore> scores = new ArrayList<>();
-
-        String query = "select vassr from VeteranAssessmentSurveyScore vassr  " +
-
+        String hql = "select vassr from VeteranAssessmentSurveyScore vassr "+
                 " where vassr.dateCompleted >= :fromDate " +
                 " and vassr.dateCompeted <= :toDate " +
+                " and vassr.clinic.id in ( :clinicIds ) "+
                 " and vassr.survey.id in ( :surveyIds ) " +
                 " order by vassr.veteran.id, vassr.survey.id, vassr.dateCompleted asc ";
 
-        return scores;
+        TypedQuery<VeteranAssessmentSurveyScore> query = entityManager.createQuery(hql, VeteranAssessmentSurveyScore.class);
 
+        query.setParameter("clinicIds", clinicIds);
+        query.setParameter("surveyIds", surveyIds);
+        query.setParameter("fromDate", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(fromDate+" 00:00:00"));
+        query.setParameter("toDate", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(toDate+" 23:59:59"));
 
+        return query.getResultList();
     }
 
+    @Override
+    public List<VeteranAssessmentSurveyScore> getDataForClicnic(List<Integer> clinicIds, List<Integer> surveyIds,
+                                                                          String fromDate, String toDate) {
+
+        String hql = "select vassr from VeteranAssessmentSurveyScore vassr "+
+                " where vassr.dateCompleted >= :fromDate " +
+                " and vassr.dateCompeted <= :toDate " +
+                " and vassr.clinic.id in ( :clinicIds ) "+
+                " and vassr.survey.id in ( :surveyIds ) " +
+                " order by vassr.clinic.id, vassr.survey.id, vassr.dateCompleted asc ";
+
+        TypedQuery<VeteranAssessmentSurveyScore> query = entityManager.createQuery(hql, VeteranAssessmentSurveyScore.class);
+
+        query.setParameter("clinicIds", clinicIds);
+        query.setParameter("surveyIds", surveyIds);
+        query.setParameter("fromDate", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(fromDate+" 00:00:00"));
+        query.setParameter("toDate", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(toDate+" 23:59:59"));
+
+        return query.getResultList();
+    }
 }
