23cb936b77f775fa43e35faac1a3a301ce1b4d6f
kjsolution <liukai@gmail.com>
2015-03-12 08:27:12 -0700

    t601: intervals sql script for t601

--- eScreeningDashboard/src/main/sql/sprint_33/01_spring33_create_score_table.sql
+++ eScreeningDashboard/src/main/sql/sprint_33/01_spring33_create_score_table.sql
@@ -38,5 +38,28 @@ insert into survey_score_interval (survey_id, min, max, meaning) values (33, '5'
 insert into survey_score_interval (survey_id, min, max, meaning) values (33, '10', '14', 'moderate anxiety');
 insert into survey_score_interval (survey_id, min, max, meaning) values (33, '15', '21', 'severe anxiety');
 
+insert into survey_score_interval (survey_id, min, max, meaning) values (36, '0', '7', 'no clinically significant insomnia');
+insert into survey_score_interval (survey_id, min, max, meaning) values (36, '8', '14', 'sub-threshold insomnia');
+insert into survey_score_interval (survey_id, min, max, meaning) values (36, '15', '21', 'moderate insomnia');
+insert into survey_score_interval (survey_id, min, max, meaning) values (36, '22', '28', 'severe insomnia');
 
+insert into survey_score_interval (survey_id, min, max, meaning) values (20, '0', '0', 'no pain');
+insert into survey_score_interval (survey_id, min, max, meaning) values (20, '1', '3', 'mild');
+insert into survey_score_interval (survey_id, min, max, meaning) values (20, '4', '5', 'moderate');
+insert into survey_score_interval (survey_id, min, max, meaning) values (20, '6', '7', 'severe');
+insert into survey_score_interval (survey_id, min, max, meaning) values (20, '8', '9', 'very severe');
+insert into survey_score_interval (survey_id, min, max, meaning) values (20, '10', '10', 'worst possible');
+
+insert into survey_score_interval (survey_id, min, max, meaning) values (23, '1', '1', 'no disability');
+insert into survey_score_interval (survey_id, min, max, meaning) values (23, '2', '2', 'mild disability');
+insert into survey_score_interval (survey_id, min, max, meaning) values (23, '3', '3', 'moderate disability');
+insert into survey_score_interval (survey_id, min, max, meaning) values (23, '4', '4', 'severe disability');
+insert into survey_score_interval (survey_id, min, max, meaning) values (23, '5', '5', 'extreme disability');
+
+insert into survey_score_interval (survey_id, min, max, meaning) values (45, '15', '18.5', 'underweight');
+insert into survey_score_interval (survey_id, min, max, meaning) values (45, '18.5', '24.99', 'normal weight');
+insert into survey_score_interval (survey_id, min, max, meaning) values (45, '25', '29.99', 'overweight');
+insert into survey_score_interval (survey_id, min, max, meaning) values (45, '30', '39', 'obese');
+insert into survey_score_interval (survey_id, min, max, meaning) values (45, '39.01', '40', 'obese');
+insert into survey_score_interval (survey_id, min, max, meaning, exception) values (45, '999', '999', 'missing', 1);
 
