e5ae52132f334e7fb0f568dc32bb4233e4eec302
kjsolution <liukai@gmail.com>
2015-03-19 08:37:41 -0700

    t597: three new reports 594/5/9 first check in.

---
+++ eScreeningDashboard/src/main/java/gov/va/escreening/dto/report/Report594DTO.java
@@ -0,0 +1,34 @@
+package gov.va.escreening.dto.report;
+
+/**
+ * Created by kliu on 3/19/15.
+ */
+public class Report594DTO {
+    private String moduleName;
+    private String modulePercent;
+    private String moduleCount;
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getModulePercent() {
+        return modulePercent;
+    }
+
+    public void setModulePercent(String modulePercent) {
+        this.modulePercent = modulePercent;
+    }
+
+    public String getModuleCount() {
+        return moduleCount;
+    }
+
+    public void setModuleCount(String moduleCount) {
+        this.moduleCount = moduleCount;
+    }
+}
