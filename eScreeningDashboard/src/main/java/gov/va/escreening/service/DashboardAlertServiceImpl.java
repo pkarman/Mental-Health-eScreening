49049c82489df02522dfaba70c04444f6eac581c
iiiming <mzhu.ctr@iiinfo.com>
2015-03-19 20:21:52 -0400

    t820 -- fixed update problem

--- eScreeningDashboard/src/main/java/gov/va/escreening/service/DashboardAlertServiceImpl.java
+++ eScreeningDashboard/src/main/java/gov/va/escreening/service/DashboardAlertServiceImpl.java
@@ -11,8 +11,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class DashboardAlertServiceImpl implements DashboardAlertService
 {
 
@@ -53,6 +55,7 @@ public class DashboardAlertServiceImpl implements DashboardAlertService
 					a.setName(name);
 					a.setMessage(name);
 					alertRepo.update(a);
+					
 				}
 			}
 			else
