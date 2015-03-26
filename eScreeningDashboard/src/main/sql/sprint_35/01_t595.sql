4a00d7ebf526691c3214fe51f497ab81f2220598
kjsolution <liukai@gmail.com>
2015-03-25 23:47:55 -0700

    t597: sql file check in.

--- eScreeningDashboard/src/main/sql/sprint_35/01_t595.sql
+++ eScreeningDashboard/src/main/sql/sprint_35/01_t595.sql
@@ -5,23 +5,23 @@ measure_id int,
 skipped int
 );
 
-CREATE TRIGGER upd_answer_presense BEFORE UPDATE ON veteran_assessment
+CREATE TRIGGER upd_question_presence BEFORE UPDATE ON veteran_assessment
 FOR EACH ROW
 BEGIN
-	if NEW.date_completed is not null AND OLD.date_completed is null then
+	IF NEW.date_completed is not null AND OLD.date_completed is null then
 		insert into veteran_assessment_question_presence
 		(veteran_assessment_id, measure_id, skipped)
-		SELECT new.veteran_assessment_id, all_measures.measure_id, ifnull(answered_measures.measure_id, -1) response
+		SELECT NEW.veteran_assessment_id, all_measures.measure_id, ifnull(answered_measures.measure_id, -1) response
                 FROM(
                     SELECT spm.survey_page_id, spm.measure_id as measure_id
                     FROM measure m
                     LEFT OUTER JOIN veteran_assessment_measure_visibility vamv ON
-                    m.measure_id=vamv.measure_id AND vamv.veteran_assessment_id= new.veteran_assessment_id
+                    m.measure_id=vamv.measure_id AND vamv.veteran_assessment_id= NEW.veteran_assessment_id
                     INNER JOIN survey_page_measure spm ON ifnull(m.parent_measure_id, m.measure_id)=spm.measure_id
                     INNER JOIN survey_page sp ON spm.survey_page_id=sp.survey_page_id
                     INNER JOIN survey s ON sp.survey_id=s.survey_id
                     INNER JOIN veteran_assessment_survey vas ON sp.survey_id=vas.survey_id
-                    WHERE vas.veteran_assessment_id = new.veteran_assessment_id
+                    WHERE vas.veteran_assessment_id = NEW.veteran_assessment_id
                     AND m.measure_type_id IN (1,2,3,4,6,7)
                     AND ifnull(vamv.is_visible, 1)
                     GROUP BY spm.survey_page_id, spm.measure_id
@@ -35,7 +35,7 @@ BEGIN
                     INNER JOIN measure m ON smr.measure_id=m.measure_id
                     INNER JOIN survey_page_measure spm on ifnull(m.parent_measure_id, m.measure_id)=spm.measure_id
                     INNER JOIN survey s ON smr.survey_id=s.survey_id
-                    WHERE smr.veteran_assessment_id = new.veteran_assessment_id
+                    WHERE smr.veteran_assessment_id = NEW.veteran_assessment_id
                     AND m.measure_type_id IN ( 1,2,3,4,6,7)
                     AND (
                         (spm.measure_id=smr.measure_id AND (smr.boolean_value = 1 OR smr.number_value IS NOT NULL
@@ -47,7 +47,7 @@ BEGIN
                             0 NOT IN (
                                     SELECT COUNT(*) FROM survey_measure_response smr2
                                     INNER JOIN measure m ON smr2.measure_id=m.measure_id
-                                    WHERE smr2.veteran_assessment_id = new.veteran_assessment_id
+                                    WHERE smr2.veteran_assessment_id = NEW.veteran_assessment_id
                                     AND m.parent_measure_id = spm.measure_id )
                             AND
                                  /* collect responses grouped by measure and tabular row. Get sum of the number of answers.
@@ -57,7 +57,7 @@ BEGIN
                                     FROM survey_measure_response smr2
                                     INNER JOIN survey s ON smr2.survey_id=s.survey_id
                                     INNER JOIN measure m ON smr2.measure_id=m.measure_id
-                                    WHERE smr2.veteran_assessment_id = new.veteran_assessment_id
+                                    WHERE smr2.veteran_assessment_id = NEW.veteran_assessment_id
                                     AND m.parent_measure_id = spm.measure_id
                                     AND m.measure_type_id IN (  1,2,3,4,6,7 )
                                     GROUP BY smr2.measure_id, smr2.tabular_row )
