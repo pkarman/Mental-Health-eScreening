1e0497c81a43fdb9e0fb33b9dd1626b05ef3b32a
iiiming <mzhu.ctr@iiinfo.com>
2015-04-11 08:37:50 -0400

    t449 -- fixed the problem with pain chart

---
+++ eScreeningDashboard/src/main/sql/sprint_35/857.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: select * from variable_template where template_id=20
+-- Date: 2015-04-11 08:21
+*/
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2301,309,'0');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2302,309,'1');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2303,309,'2');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2304,309,'3');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2305,309,'4');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2306,309,'5');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2307,309,'6');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2308,309,'7');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2309,309,'8');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2310,309,'9');
+INSERT INTO `variable_template` (`assessment_variable_id`,`template_id`,`override_display_value`) VALUES (2311,309,'10');
