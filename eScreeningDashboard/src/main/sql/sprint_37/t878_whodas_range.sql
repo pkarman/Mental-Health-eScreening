0fb6bb1f24ac6a47d0d9d0e92a338fd7d368e714
Khalid R. Rizvi <javaguru786@gmail.com>
2015-05-11 10:50:11 -0400

    t878 fixed range for WHODAS

---
+++ eScreeningDashboard/src/main/sql/sprint_37/t878_whodas_range.sql
@@ -0,0 +1,5 @@
+UPDATE survey_score_interval SET min='0.00', max='1.00' WHERE id='28';
+UPDATE survey_score_interval SET min='1.01', max='2.00' WHERE id='29';
+UPDATE survey_score_interval SET min='2.01', max='3.00' WHERE id='30';
+UPDATE survey_score_interval SET min='3.01', max='4.00' WHERE id='31';
+UPDATE survey_score_interval SET min='4.01', max='5.00' WHERE id='32';
