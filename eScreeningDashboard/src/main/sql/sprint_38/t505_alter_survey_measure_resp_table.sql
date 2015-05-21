e969bec3be8c3dbc04fa0ff616233506068a379e
iiiming <mzhu.ctr@iiinfo.com>
2015-05-20 22:54:04 -0400

    t505-fix the sql script

--- eScreeningDashboard/src/main/sql/sprint_38/t505_alter_survey_measure_resp_table.sql
+++ eScreeningDashboard/src/main/sql/sprint_38/t505_alter_survey_measure_resp_table.sql
@@ -1,8 +1,8 @@
-ALTER TABLE `escreening`.`survey_measure_response` 
+ALTER TABLE `survey_measure_response` 
 ADD COLUMN `copiedFromAssessment` INT NULL AFTER `date_modified`;
-ALTER TABLE `escreening`.`survey_measure_response` 
+ALTER TABLE `survey_measure_response` 
 ADD CONSTRAINT `fk_survey_meas_resp_va_copy`
   FOREIGN KEY (`copiedFromAssessment`)
-  REFERENCES `escreening`.`veteran_assessment` (`veteran_assessment_id`)
+  REFERENCES `veteran_assessment` (`veteran_assessment_id`)
   ON DELETE SET NULL
   ON UPDATE NO ACTION;
