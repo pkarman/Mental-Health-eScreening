fc8d02aa05f030f0832d3e23ef568a709dd292d1
Khalid R. Rizvi <javaguru786@gmail.com>
2015-06-21 00:56:29 -0400

    t941 replaced DAT_score to fix the float data type with integer

--- eScreeningDashboard/src/main/sql/sprint_prototype_bug_fix/t941.sql
+++ eScreeningDashboard/src/main/sql/sprint_prototype_bug_fix/t941.sql
@@ -1,4 +1,6 @@
 -- The syntax for how we call functions in our spel expressions changed in 643, this formula now calls it correctly
-UPDATE `escreening`.`assessment_variable` SET `formula_template`='calculateAge(\"[143]\")' WHERE `assessment_variable_id`='12';
+UPDATE assessment_variable SET formula_template='calculateAge(\"[143]\")' WHERE assessment_variable_id='12';
 -- The use of a override value was causing template 4 to not work correctly
 UPDATE variable_template SET override_display_value=null WHERE variable_template_id=2950;
+
+UPDATE assessment_variable SET formula_template='([1000] + [1001] + [1003] + [1004] + [1005] + [1006] + [1007] + [1008] + [1009] + T(Math).min([1000],(1 - [1002])))' WHERE assessment_variable_id='1010';
