f16c18c7c585250d4670c3b5f138a06591771aa8
Robin Carnow <rcarnow@iiinfo.com>
2015-07-31 15:08:24 -0400

    t944 Listed content that should be filtered/removed from all content extracted

---
+++ eScreeningDashboard/src/main/sql/deployment_content/3_remove_sdc_content.sql
@@ -0,0 +1,22 @@
+
+/*
+remove batteries:     
+    Aspire Discharge Battery (ID 3)
+    Aspire Intake Battery (ID 1)
+    Aspire Progress Monitoring Battery (ID 2)
+    Women’s Assessment Battery (ID 7)
+    PTSD Progress – Jacobus (ID 8)
+    Lowther Nurse Case Management Battery (ID 10)
+*/
+
+
+
+/*
+Remove modules:
+    939 (ID 67)
+    DAST 10 Test (ID 65)
+    BDI-II (in progress) (ID 59)
+    DELETE ME (ID 63)
+    eScreening Quality Assessment (ID 64)
+*/
+
