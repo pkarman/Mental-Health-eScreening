2523e7d8d583d284c467b2095742c93c924ecc8b
iiiming <mzhu.ctr@iiinfo.com>
2015-10-02 13:04:04 -0400

    change hf'ien to null instead of delete, to preserver exising assessment's HF relationship

--- escreening/src/main/java/gov/va/escreening/service/VistaServiceImpl.java
+++ escreening/src/main/java/gov/va/escreening/service/VistaServiceImpl.java
@@ -736,7 +736,8 @@ public class VistaServiceImpl implements VistaService {
         for(HealthFactor hf : hfList)
         {
             eventService.deleteEventForHealthFactor(hf);
-            healthFactorRepo.delete(hf);
+            hf.setVistaIen(null);
+            healthFactorRepo.update(hf);
             logger.info("Removed Health Factor " + hf.getName() + hf.getVistaIen());
         }
 
